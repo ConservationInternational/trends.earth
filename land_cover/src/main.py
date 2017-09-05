94232bb0e4520f77b856d524b610775a38a0d882
Alex Zvoleff <azvoleff@gmail.com>
2017-09-05 00:13:51 -0400

    Fix result URL formatting.

--- land_cover/src/main.py
+++ land_cover/src/main.py
@@ -114,7 +114,8 @@ def land_cover(year_bl_start, year_bl_end, year_target, geojson, EXECUTION_ID,
     cloud_datasets = []
     for task in tasks:
         task.join()
-        cloud_datasets.append(CloudDataset('geotiff', task.name, ['{}_{}'.format(EXECUTION_ID, task.name)]))
+        results_url = CloudUrl('{}_{}'.format(EXECUTION_ID, task.name))
+        cloud_datasets.append(CloudDataset('geotiff', task.name, [results_url]))
 
     logger.debug("Setting up results JSON.")
     gee_results = GEEResults('land_cover', cloud_datasets)

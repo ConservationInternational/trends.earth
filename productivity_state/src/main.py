b45b8e26ba0efaf77467a96ca5eb84a57e419b95
Alex Zvoleff <azvoleff@gmail.com>
2017-12-01 15:13:35 -0500

    Don't mask productivity with land cover.

--- productivity_state/src/main.py
+++ productivity_state/src/main.py
@@ -27,19 +27,6 @@ def productivity_state(year_init_bl_start, year_init_bl_end,
 
     ndvi_1yr = ee.Image(ndvi_gee_dataset)
 
-    # land cover data from esa cci
-    lc = ee.Image("users/geflanddegradation/toolbox_datasets/lcov_esacc_1992_2015")
-    # Handle case of end year that isn't included in the CCI data
-    year_end_esa_cci = year_init_tg_end
-    if year_init_tg_end > 2015:
-        year_end_esa_cci = 2015
-    elif year_init_tg_end < 1992:
-        year_end_esa_cci = 1992
-    # select lc for final map and resample to ndvi data resolution
-    lc_proj_esa = lc.select('y{}'.format(year_end_esa_cci)). \
-            reduceResolution(reducer=ee.Reducer.mode(), maxPixels=2000). \
-            reproject(crs=ndvi_1yr.projection())
-
     # compute min and max of annual ndvi for the baseline period
     bl_ndvi_range = ndvi_1yr.select(ee.List.sequence(bl_str,bl_end).map(function(i) {return ee.String('y').cat(ee.Number(i).int())})). \
                       reduce(ee.Reducer.percentile([0,100]));
@@ -57,14 +44,6 @@ def productivity_state(year_init_bl_start, year_init_bl_end,
             .reduce(ee.Reducer.mean()).rename(['ndvi'])
     tg_ndvi_mean = ndvi_1yr.select(ee.List(['y{}'.format(i) for i in range(year_init_tg_start, year_init_tg_end + 1)])) \
             .reduce(ee.Reducer.mean()).rename(['ndvi'])
-
-    # initial degradation
-    # reclassify mean ndvi for baseline period based on 50th percentile. -1 is 
-    # degreaded, 0 is not degreade, 2 is water ,and 3 is urban
-    #ini_deg = ee.Image(-2).where(bl_ndvi_mean.lt(bl_ndvi_perc.select('p50')), -1) \
-    #        .where(bl_ndvi_mean.gte(bl_ndvi_perc.select('p50')), 0) \
-    #        .where(lc_proj_esa.eq(210), 9998) \
-    #        .where(lc_proj_esa.eq(190), 9999)
 
     # emerging degradation
     # reclassify mean ndvi for baseline period based on the percentiles
@@ -95,9 +74,7 @@ def productivity_state(year_init_bl_start, year_init_bl_end,
     # degreaded, -1 is degraded, 1 is degraded, 9998 is water, and 9999 is urban
     eme_deg = ee.Image(9997).where(classes_chg.lte(-2),-1) \
           .where(classes_chg.gte(-1).And(classes_chg.lte( 1)), 0) \
-          .where(classes_chg.gte( 2), 1) \
-          .where(lc_proj_esa.eq(210),9998) \
-          .where(lc_proj_esa.eq(190),9999)
+          .where(classes_chg.gte( 2), 1)
 
     tasks = []
     #tasks.append(util.export_to_cloudstorage(ini_deg.int16(), 

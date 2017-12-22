6109f73827f591bb00dc7fefbc68b610610a0e66
Alex Zvoleff <azvoleff@gmail.com>
2017-12-21 22:41:33 -0500

    Add initial SOC code.

--- soil_organic_carbon/src/main.py
+++ soil_organic_carbon/src/main.py
@@ -19,41 +19,102 @@ from landdegradation import GEEIOError
 
 from landdegradation.schemas import GEEResults, CloudDataset, CloudUrl, GEEResultsSchema
 
-def soc(year_bl_start, year_bl_end, year_target, geojson, remap_matrix, 
+
+def soc(year_bl_start, year_bl_end, year_target, geojson, remap_matrix,
         EXECUTION_ID, logger):
     """
     Calculate SOC indicator.
     """
     logger.debug("Entering soc function.")
 
-    # TODO: code the SOC indicator. Right now this function just returns the 
-    # soilgrids 0-30cm SOC data for use in the UNCCD reporting function in 
-    # trends.earth.
+    regime   = 'temp_dry' # climate regime: 'temp_dry', 'temp_moist', 'trop_dry', 'trop_moist', 'trop_mont'
 
-    # ## land cover
-    # lc = ee.Image("users/geflanddegradation/toolbox_datasets/lcov_esacc_1992_2015")
-    #
-    # ## target land cover map reclassified to IPCC 6 classes
-    # lc_tg = lc.select('y{}'.format(year_target))\
-    #         .remap(remap_matrix[0], remap_matrix[1])
-    #
-    # ## baseline land cover map reclassified to IPCC 6 classes
-    # lc_bl = lc.select(ee.List.sequence(year_bl_start - 1992, year_bl_end - 1992, 1))\
-    #         .reduce(ee.Reducer.mode())\
-    #         .remap(remap_matrix[0], remap_matrix[1])
-    #
-    # ## compute transition map (first digit for baseline land cover, and second digit for target year land cover)
-    # lc_tr = lc_bl.multiply(10).add(lc_tg)
+    if (regime == 'temp_dry')   {fl = 0.80}
+    if (regime == 'temp_moist') {fl = 0.69}
+    if (regime == 'trop_dry')   {fl = 0.58}
+    if (regime == 'trop_moist') {fl = 0.48}
+    if (regime == 'trop_mont')  {fl = 0.64}
+
+    ## land cover
+    lc = ee.Image("users/geflanddegradation/toolbox_datasets/lcov_esacc_1992_2015")
+
+    ## target land cover map reclassified to IPCC 6 classes
+    lc_tg = lc.select('y{}'.format(year_target))\
+            .remap(remap_matrix[0], remap_matrix[1])
+
+    ## baseline land cover map reclassified to IPCC 6 classes
+    lc_bl = lc.select(ee.List.sequence(year_bl_start - 1992, year_bl_end - 1992, 1))\
+            .reduce(ee.Reducer.mode())\
+            .remap(remap_matrix[0], remap_matrix[1])
+
+    ## compute transition map (first digit for baseline land cover, and second digit for target year land cover)
+    lc_tr = lc_bl.multiply(10).add(lc_tg)
 
     ## soc
     soc = ee.Image("users/geflanddegradation/toolbox_datasets/soc_sgrid_30cm")
+    soc_ini = soc.updateMask(soc.neq(-32768))
 
-    #lc_out = lc_bl.addBands(lc_tg).addBands(lc_tr).addBands(lc_dg).addBands(soc)
-    lc_out = soc
+    # stock change factor for land use
+    lc_tr_fl = lc_tr.remap([11,  12,  13,  14,  15,   16,  17,
+                            21,  22,  23,  24,  25,   26,  27,
+                            31,  32,  33,  34,  35,   36,  37,
+                            41,  42,  43,  44,  45,   46,  47,
+                            51,  52,  53,  54,  55,   56,  57,
+                            61,  62,  63,  64,  65,   66,  67,
+                            71,  72,  73,  74,  75,   76,  77],
+                           [ 1,   1,  fl,   1, 0.1,  0.1,   0,
+                             1,   1,  fl,   1, 0.1,  0.1,   0,
+                             1/fl,1/fl,1, 1/0.71, 0.1, 0.1, 0,
+                             1,   1, 0.71,  1, 0.1,  0.1,   0,
+                             10, 10,  10,  10,   1,    1,   0,
+                             10, 10,  10,  10,   1,    1,   0,
+                              0,  0,   0,   0,   0,    0,   0])
 
-    task = util.export_to_cloudstorage(lc_out.int16(), 
-            lc_out.projection(), geojson, 'soc', logger, 
-            EXECUTION_ID)
+    # stock change factor for management regime
+    lc_tr_fm = lc_tr.remap([11, 12, 13, 14, 15, 16, 17,
+                                21, 22, 23, 24, 25, 26, 27,
+                                31, 32, 33, 34, 35, 36, 37,
+                                41, 42, 43, 44, 45, 46, 47,
+                                51, 52, 53, 54, 55, 56, 57,
+                                61, 62, 63, 64, 65, 66, 67,
+                                71, 72, 73, 74, 75, 76, 77], 
+                               [ 1,  1,  1,  1,  1,  1,  1,
+                                 1,  1,  1,  1,  1,  1,  1,
+                                 1,  1,  1,  1,  1,  1,  1,
+                                 1,  1,  1,  1,  1,  1,  1,
+                                 1,  1,  1,  1,  1,  1,  1,
+                                 1,  1,  1,  1,  1,  1,  1,
+                                 1,  1,  1,  1,  1,  1,  1])
+
+    # stock change factor for input of organic matter
+    lc_tr_fo = lc_tr.remap([11, 12, 13, 14, 15, 16, 17,
+                                21, 22, 23, 24, 25, 26, 27,
+                                31, 32, 33, 34, 35, 36, 37,
+                                41, 42, 43, 44, 45, 46, 47,
+                                51, 52, 53, 54, 55, 56, 57,
+                                61, 62, 63, 64, 65, 66, 67,
+                                71, 72, 73, 74, 75, 76, 77], 
+                               [ 1,  1,  1,  1,  1,  1,  1,
+                                 1,  1,  1,  1,  1,  1,  1,
+                                 1,  1,  1,  1,  1,  1,  1,
+                                 1,  1,  1,  1,  1,  1,  1,
+                                 1,  1,  1,  1,  1,  1,  1,
+                                 1,  1,  1,  1,  1,  1,  1,
+                                 1,  1,  1,  1,  1,  1,  1])
+
+    soc_fin = soc_ini.subtract((((soc_ini.subtract((soc_ini.
+                                                    multiply(lc_tr_fl).
+                                                    multiply(lc_tr_fm).
+                                                    multiply(lc_tr_fo)))).
+                                  divide(20)).multiply(year_target-year_bl_start)))
+
+    soc_pch = ((soc_fin.subtract(soc_ini)).divide(soc_ini)).multiply(100)
+
+    soc_out = soc_ini.addBands(soc_fin).addBands(soc_pch)
+
+    task = util.export_to_cloudstorage(soc_out.int16(),
+                                       soc_out.projection(), geojson, 'soc', logger,
+                                       EXECUTION_ID)
     task.join()
 
     logger.debug("Setting up results JSON.")
@@ -64,6 +125,7 @@ def soc(year_bl_start, year_bl_end, year_target, geojson, remap_matrix,
 
     return json_results
 
+
 def run(params, logger):
     """."""
     logger.debug("Loading parameters.")
@@ -71,12 +133,12 @@ def run(params, logger):
     year_bl_end = params.get('year_bl_end', 2015)
     year_target = params.get('year_target', 2015)
     geojson = params.get('geojson', util.tza_geojson)
-    remap_matrix_default = [[10, 11, 12, 20, 30, 40, 50, 60, 61, 62, 70, 71, 72, 
-                             80, 81, 82, 90, 100, 110, 120, 121, 122, 130, 140, 
-                             150, 151, 152, 153, 160, 170, 180, 190, 200, 201, 
+    remap_matrix_default = [[10, 11, 12, 20, 30, 40, 50, 60, 61, 62, 70, 71, 72,
+                             80, 81, 82, 90, 100, 110, 120, 121, 122, 130, 140,
+                             150, 151, 152, 153, 160, 170, 180, 190, 200, 201,
                              202, 210, 220],
-                            [3, 3, 3, 3, 3, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
-                             1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 4, 4, 5, 6, 6, 
+                            [3, 3, 3, 3, 3, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                             1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 4, 4, 5, 6, 6,
                              6, 7, 6]]
     remap_matrix = params.get('remap_matrix', remap_matrix_default)
 
@@ -96,7 +158,7 @@ def run(params, logger):
         EXECUTION_ID = params.get('EXECUTION_ID', None)
 
     logger.debug("Running main script.")
-    json_results = soc(year_bl_start, year_bl_end, year_target, geojson, 
+    json_results = soc(year_bl_start, year_bl_end, year_target, geojson,
                        remap_matrix, EXECUTION_ID, logger)
 
     return json_results.data

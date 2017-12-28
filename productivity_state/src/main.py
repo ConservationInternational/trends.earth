4db80f949c489dd1a04d66fd2d5d676d49204712
Alex Zvoleff <azvoleff@gmail.com>
2017-12-28 15:11:26 -0500

    Pep8, and also output number of changes in class.

--- productivity_state/src/main.py
+++ productivity_state/src/main.py
@@ -19,9 +19,10 @@ from landdegradation import GEEIOError
 
 from landdegradation.schemas import GEEResults, CloudDataset, CloudUrl, GEEResultsSchema
 
-def productivity_state(year_bl_start, year_bl_end, 
-        year_tg_start, year_tg_end,
-        ndvi_gee_dataset, geojson, EXECUTION_ID, logger):
+
+def productivity_state(year_bl_start, year_bl_end,
+                       year_tg_start, year_tg_end,
+                       ndvi_gee_dataset, geojson, EXECUTION_ID, logger):
 
     logger.debug("Entering productivity_state function.")
 
@@ -29,21 +30,21 @@ def productivity_state(year_bl_start, year_bl_end,
 
     # compute min and max of annual ndvi for the baseline period
     bl_ndvi_range = ndvi_1yr.select(ee.List(['y{}'.format(i) for i in range(year_bl_start, year_bl_end + 1)])) \
-            .reduce(ee.Reducer.percentile([0, 100]))
-    
+        .reduce(ee.Reducer.percentile([0, 100]))
+
     # add two bands to the time series: one 5% lower than min and one 5% higher than max
     bl_ndvi_ext = ndvi_1yr.select(ee.List(['y{}'.format(i) for i in range(year_tg_start, year_tg_end + 1)])) \
-            .addBands(bl_ndvi_range.select('p0').subtract((bl_ndvi_range.select('p100').subtract(bl_ndvi_range.select('p0'))).multiply(0.05)))\
-            .addBands(bl_ndvi_range.select('p100').add((bl_ndvi_range.select('p100').subtract(bl_ndvi_range.select('p0'))).multiply(0.05)))
-            
+        .addBands(bl_ndvi_range.select('p0').subtract((bl_ndvi_range.select('p100').subtract(bl_ndvi_range.select('p0'))).multiply(0.05)))\
+        .addBands(bl_ndvi_range.select('p100').add((bl_ndvi_range.select('p100').subtract(bl_ndvi_range.select('p0'))).multiply(0.05)))
+
     # compute percentiles of annual ndvi for the extended baseline period
-    bl_ndvi_perc = bl_ndvi_ext.reduce(ee.Reducer.percentile([10,20,30,40,50,60,70,80,90,100]))
+    bl_ndvi_perc = bl_ndvi_ext.reduce(ee.Reducer.percentile([10, 20, 30, 40, 50, 60, 70, 80, 90, 100]))
 
     # compute mean ndvi for the baseline and target period period
     bl_ndvi_mean = ndvi_1yr.select(ee.List(['y{}'.format(i) for i in range(year_bl_start, year_bl_end + 1)])) \
-            .reduce(ee.Reducer.mean()).rename(['ndvi'])
+        .reduce(ee.Reducer.mean()).rename(['ndvi'])
     tg_ndvi_mean = ndvi_1yr.select(ee.List(['y{}'.format(i) for i in range(year_tg_start, year_tg_end + 1)])) \
-            .reduce(ee.Reducer.mean()).rename(['ndvi'])
+        .reduce(ee.Reducer.mean()).rename(['ndvi'])
 
     # reclassify mean ndvi for baseline period based on the percentiles
     bl_classes = ee.Image(9999) \
@@ -56,7 +57,7 @@ def productivity_state(year_bl_start, year_bl_end,
         .where(bl_ndvi_mean.gt(bl_ndvi_perc.select('p60')), 7) \
         .where(bl_ndvi_mean.gt(bl_ndvi_perc.select('p70')), 8) \
         .where(bl_ndvi_mean.gt(bl_ndvi_perc.select('p80')), 9) \
-        .where(bl_ndvi_mean.gt(bl_ndvi_perc.select('p90')),10)
+        .where(bl_ndvi_mean.gt(bl_ndvi_perc.select('p90')), 10)
 
     # reclassify mean ndvi for target period based on the percentiles
     tg_classes = ee.Image(9999) \
@@ -69,22 +70,25 @@ def productivity_state(year_bl_start, year_bl_end,
         .where(tg_ndvi_mean.gt(bl_ndvi_perc.select('p60')), 7) \
         .where(tg_ndvi_mean.gt(bl_ndvi_perc.select('p70')), 8) \
         .where(tg_ndvi_mean.gt(bl_ndvi_perc.select('p80')), 9) \
-        .where(tg_ndvi_mean.gt(bl_ndvi_perc.select('p90')),10)
+        .where(tg_ndvi_mean.gt(bl_ndvi_perc.select('p90')), 10)
 
     # difference between start and end clusters >= 2
     classes_chg = tg_classes.subtract(bl_classes)
-    # create final degradation output layer (9999 is background), 0 is not 
+    # create final degradation output layer (9999 is background), 0 is not
     # degraded, -1 is degraded, 1 is degraded
-    eme_deg = ee.Image(9999).where(classes_chg.lte(-2),-1) \
-          .where(classes_chg.gte(-1).And(classes_chg.lte(1)), 0) \
-          .where(classes_chg.gte( 2), 1)
+    deg = ee.Image(9999).where(classes_chg.lte(-2), -1) \
+        .where(classes_chg.gte(-1).And(classes_chg.lte(1)), 0) \
+        .where(classes_chg.gte(2), 1)
 
-    task = util.export_to_cloudstorage(eme_deg.int16(), 
-            ndvi_1yr.projection(), geojson, 'prod_state', logger, 
-            EXECUTION_ID)
+    out = deg.addBands(classes_chg)
+
+    task = util.export_to_cloudstorage(out.int16(),
+                                       ndvi_1yr.projection(), geojson, 'prod_state', logger,
+                                       EXECUTION_ID)
     task.join()
 
     return task.url()
+
 
 def run(params, logger):
     """."""
@@ -112,8 +116,8 @@ def run(params, logger):
         EXECUTION_ID = params.get('EXECUTION_ID', None)
 
     logger.debug("Running main script.")
-    url = productivity_state(year_bl_start, year_bl_end, year_tg_start, 
-            year_tg_end, ndvi_gee_dataset, geojson, EXECUTION_ID, logger)
+    url = productivity_state(year_bl_start, year_bl_end, year_tg_start,
+                             year_tg_end, ndvi_gee_dataset, geojson, EXECUTION_ID, logger)
 
     logger.debug("Setting up results JSON.")
     cloud_dataset = CloudDataset('geotiff', 'prod_state', [CloudUrl(url)])

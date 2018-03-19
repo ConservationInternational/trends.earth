57cd26c49e24859bbec9ebc05a4c58df2fa24722
Alex Zvoleff <azvoleff@gmail.com>
2018-03-19 06:57:22 -0400

    Bump version.

--- LDMP/help/source/conf.py
+++ LDMP/help/source/conf.py
@@ -55,9 +55,9 @@ gettext_compact = False
 # built documents.
 #
 # The short X.Y version.
-version = '0.50'
+version = '0.52'
 # The full version, including alpha/beta/rc tags.
-release = '0.50'
+release = '0.52'
 
 rst_epilog = """
 .. |iconCalculator| image:: /static/common/icon-calculator.png

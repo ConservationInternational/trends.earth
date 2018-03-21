04d3b5cb6e4cd47d38f53820482c42061834a601
Alex Zvoleff <azvoleff@gmail.com>
2018-03-21 15:18:08 -0400

    Fix tests.

--- LDMP/test/dialog_settings_tests.py
+++ LDMP/test/dialog_settings_tests.py
@@ -7,44 +7,57 @@ import unittest
 import sys
 import os
 from qgis.core import *
+from PyQt4.QtGui import QMessageBox
 from PyQt4.QtCore import *
 from PyQt4.QtTest import QTest
-from LDMP import DlgSettings()
+from LDMP.settingsi import DlgSettingsLogin
 
+
+def close_msg_boxes():
+    topWidgets = QApplication.topLevelWidgets();
+    for w in topWidgets:
+        if isinstance(w, QMessageBox):
+            QTest.keyClick(w, Qt.Key_Enter)
 
 class DialogTestsAPI(unittest.TestCase):
-    def testAPILogin(self):
-        dialog = DefineCatalogDialog(self.explorer.catalogs())
-        dialog.nameBox.setText("trends-earth-automated-tests@trends.earth")
-        dialog.urlBox.setText("http://" + geoserverLocation() + "/geoserver")
-        dialog.passwordBox.setText("password")
-        dialog.usernameBox.setText("username")
-        okWidget = dialog.buttonBox.button(dialog.buttonBox.Ok)
-        QTest.mouseClick(okWidget, Qt.LeftButton)
-        self.assertTrue(dialog.ok)
-        self.assertEquals("username", dialog.username)
-        self.assertEquals("password", dialog.password)
-        self.assertEquals("name", dialog.name)
-        self.assertEquals("http://" + geoserverLocation() + "/geoserver/rest", dialog.url)
-        settings = QSettings()
-        settings.endGroup()
-        settings.beginGroup("/GeoServer/Catalogs/name")
-        settings.remove("")
-        settings.endGroup()
+    with open(os.path.join(os.path.dirname(__file__), 'aws_credentials.json'), 'r') as fin:
+        keys = json.load(fin)
 
-        QTest.mouseClick(okWidget, Qt.LeftButton)
-        self.assertTrue(dialog.ok)
-        self.assertEquals("catalogname", dialog.name)
-        self.assertEquals("http://localhost:8081/geoserver/rest", dialog.url)
-        dialog = DefineCatalogDialog(self.explorer.catalogs())
-        self.assertEquals("catalogname", dialog.nameBox.text())
-        self.assertEquals("localhost:8081/geoserver", dialog.urlBox.text())
+    def testAPILogin(self):
+        dialog = DlgSettingsLogin()
+        dialog.email.setText(keys['email'])
+        dialog.password.setText(keys['password'])
+        self.assertEquals(keys['email'], dialog.username)
+        self.assertEquals(keys['password'], dialog.password)
         okWidget = dialog.buttonBox.button(dialog.buttonBox.Ok)
         QTest.mouseClick(okWidget, Qt.LeftButton)
+        self.assertTrue(dialog.ok)
+
+        ret = dialog.login()
+        self.assertTrue(ret)
+        close_msg_boxes()
+
+        dialog.email.setText('')
+        dialog.password.setText(keys['password'])
+        ret = dialog.login()
+        self.assertFalse(ret)
+        close_msg_boxes()
+
+        dialog.email.setText(keys['email'])
+        dialog.password.setText('')
+        ret = dialog.login()
+        self.assertFalse(ret)
+        close_msg_boxes()
+
+        dialog.email.setText('')
+        dialog.password.setText('')
+        ret = dialog.login()
+        self.assertFalse(ret)
+        close_msg_boxes()
 
 def suite():
     suite = unittest.TestSuite()
-    suite.addTests(unittest.makeSuite(GSNameDialogTest, 'test'))
+    suite.addTests(unittest.makeSuite(DialogTestsAPI, 'test'))
     return suite
 
 def run_all():

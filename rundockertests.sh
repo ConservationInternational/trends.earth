fdc8910129cb6ab580ceb64591b77209d11e4a35
Alex Zvoleff <azvoleff@gmail.com>
2018-03-23 18:07:27 -0400

    Run on local machine.

--- rundockertests.sh
+++ rundockertests.sh
@@ -2,14 +2,12 @@
 # Run docker tests on your local machine
 
 PLUGIN_NAME="LDMP"
-export QGIS_VERSION_TAG="master_2"
 
-docker-compose down -v
-docker-compose up -d
+DOCKER_RUN_COMMAND="docker exec -it qgis-testing-environment sh -c"
+
+git clone https://github.com/boundlessgeo/qgis-testing-environment-docker qgis-testing-environment
+cd qgis-testing-environment && docker build -t qgis-testing-environment --build-arg QGIS_BRANCH=release-2_18 --build-arg LEGACY='true' .
 sleep 10
-
-
-DOCKER_RUN_COMMAND="docker-compose exec qgis-testing-environment sh -c"
 
 # Setup
 $DOCKER_RUN_COMMAND "qgis_setup.sh $PLUGIN_NAME"

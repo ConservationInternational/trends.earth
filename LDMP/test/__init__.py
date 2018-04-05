71775ea27e1ff638e9c89f944ec7888a39a45b56
Alex Zvoleff <azvoleff@gmail.com>
2018-04-05 09:54:12 -0400

    Move message box closing to __init__

--- LDMP/test/__init__.py
+++ LDMP/test/__init__.py
@@ -15,7 +15,21 @@
 import os
 import json
 
+from PyQt4.QtCore import QTimer
+from PyQt4.QtGui import QMessageBox, QApplication
+from PyQt4.QtTest import QTest
+
 with open(os.path.join(os.path.dirname(__file__), 'trends.earth_test_user_credentials.json'), 'r') as fin:
     regular_keys = json.load(fin)
 with open(os.path.join(os.path.dirname(__file__), 'trends.earth_admin_user_credentials.json'), 'r') as fin:
     admin_keys = json.load(fin)
+
+# Ensure any message boxes that open are closed within 5 seconds
+def close_msg_boxes():
+    for w in QApplication.topLevelWidgets():
+        if isinstance(w, QMessageBox):
+            print('Closing message box')
+            QTest.keyClick(w, Qt.Key_Enter)
+timer = QTimer()
+timer.timeout.connect(close_msg_boxes)
+timer.start(5000)

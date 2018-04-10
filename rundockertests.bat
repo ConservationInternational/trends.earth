aceee1ca3868ae68456ea39707350f041f37c951
Alex Zvoleff <azvoleff@gmail.com>
2018-04-10 12:00:11 -0400

    Need the credential files.

--- rundockertests.bat
+++ rundockertests.bat
@@ -3,8 +3,9 @@ REM Run docker tests on your local machine
 
 set PLUGIN_NAME="LDMP"
 set QGIS_VERSION_TAG=master_2
+set CONTAINER=trendsearth_qgis-testing-environment_1
  
-set DOCKER_RUN_COMMAND=docker exec -it trendsearth_qgis-testing-environment_1 sh -c
+set DOCKER_RUN_COMMAND=docker exec -it %CONTAINER% sh -c
 
 docker-compose down -v
 docker-compose up -d
@@ -17,6 +18,8 @@ REM Setup docker instance
 %DOCKER_RUN_COMMAND% "cd /tests_directory && git submodule update --init --recursive"
 %DOCKER_RUN_COMMAND% "pip install paver"
 %DOCKER_RUN_COMMAND% "pip install boto3"
+docker cp trends.earth_test_user_credentials.json %CONTAINER%:/tests_directory/LDMP/test/trends.earth_test_user_credentials.json
+docker cp trends.earth_admin_user_credentials.json %CONTAINER%:/tests_directory/LDMP/test/trends.earth_admin_user_credentials.json
 %DOCKER_RUN_COMMAND% "cd /tests_directory && paver setup && paver package --tests"
 
 REM Run the tests

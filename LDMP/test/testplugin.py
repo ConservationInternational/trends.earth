8f16a9aeb68421ea3f8fa9135edf44bc44008b01
Alex Zvoleff <azvoleff@gmail.com>
2018-04-11 09:53:52 -0400

    Actually fix module name.

--- LDMP/test/testplugin.py
+++ LDMP/test/testplugin.py
@@ -1,12 +1,12 @@
 import unittest
 import sys
 
-from LDMP.test.test_dialog_settings import SettingSuite
+from LDMP.test.test_dialog_settings import SettingsSuite
 
 def unitTests():
     _tests = []
-    _tests.extend(SettingSuite())
+    _tests.extend(SettingsSuite())
     return _tests
 
 def run_all():
-    unittest.TextTestRunner(verbosity=3, stream=sys.stdout).run(SettingSuite())
+    unittest.TextTestRunner(verbosity=3, stream=sys.stdout).run(SettingsSuite())

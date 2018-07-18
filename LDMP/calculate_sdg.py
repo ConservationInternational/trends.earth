93435fd9d49eb0817593c29856eeced25fdc8840
Alex Zvoleff <azvoleff@gmail.com>
2018-07-18 18:24:44 -0400

    Moves and renames for clarity, TC summary.

--- LDMP/calculate_sdg.py
+++ LDMP/calculate_sdg.py
@@ -39,6 +39,8 @@ from LDMP.schemas.schemas import BandInfo, BandInfoSchema
 from LDMP.gui.DlgCalculateOneStep import Ui_DlgCalculateOneStep
 from LDMP.gui.DlgCalculateSummaryTableAdmin import Ui_DlgCalculateSummaryTableAdmin
 from LDMP.worker import AbstractWorker, StartWorker
+from LDMP.summary import xtab, merge_xtabs, calc_total_table, calc_area_table, \
+    merge_area_tables, get_xtab_area
 
 
 class DlgCalculateOneStep(DlgCalculateBase, Ui_DlgCalculateOneStep):
@@ -597,127 +599,6 @@ class DegradationSummaryWorkerSDG(AbstractWorker):
 
             return list((soc_totals_table, lc_totals_table, trans_xtab, 
                          sdg_tbl_overall, sdg_tbl_prod, sdg_tbl_soc, sdg_tbl_lc))
-
-
-def xtab(*cols):
-    # Based on https://gist.github.com/alexland/d6d64d3f634895b9dc8e, but
-    # modified to ignore np.nan
-    if not all(len(col) == len(cols[0]) for col in cols[1:]):
-        raise ValueError("all arguments must be same size")
-
-    if len(cols) == 0:
-        raise TypeError("xtab() requires at least one argument")
-
-    def fnx1(q): return len(q.squeeze().shape)
-    if not all([fnx1(col) == 1 for col in cols]):
-        raise ValueError("all input arrays must be 1D")
-
-    # Filter na values out of all columns
-    nafilter = ~np.any(np.isnan(cols), 0)
-
-    headers, idx = zip(*(np.unique(col[nafilter], return_inverse=True) for col in cols))
-    shape_xt = [uniq_vals_col.size for uniq_vals_col in headers]
-    xt = np.zeros(shape_xt)
-    np.add.at(xt, idx, 1)
-
-    return list((headers, xt))
-
-
-def merge_xtabs(tab1, tab2):
-    """Mergies two crosstabs - allows for block-by-block crosstabs"""
-    headers = tuple(np.array(np.unique(np.concatenate(header))) for header in zip(tab1[0], tab2[0]))
-    shape_xt = [uniq_vals_col.size for uniq_vals_col in headers]
-    # Make this array flat since it will be used later with ravelled indexing
-    xt = np.zeros(np.prod(shape_xt))
-
-    # This handles combining a crosstab from a new block with an existing one
-    # that has been maintained across blocks
-    def add_xt_block(xt_bl):
-        col_ind = np.tile(tuple(np.where(headers[0] == item) for item in xt_bl[0][0]), xt_bl[0][1].size)
-        row_ind = np.transpose(np.tile(tuple(np.where(headers[1] == item) for item in xt_bl[0][1]), xt_bl[0][0].size))
-        ind = np.ravel_multi_index((col_ind, row_ind), shape_xt)
-        np.add.at(xt, ind.ravel(), xt_bl[1].ravel())
-    add_xt_block(tab1)
-    add_xt_block(tab2)
-
-    return list((headers, xt.reshape(shape_xt)))
-
-
-def calc_total_table(a_trans, a_soc, total_table, cell_area):
-    """Calculates an total table for an array"""
-    if total_table:
-        # Add in totals for past total_table if one is provided
-        transitions = np.unique(np.concatenate([a_trans.ravel(), total_table[0]]))
-        ind = np.concatenate(tuple(np.where(transitions == item)[0] for item in total_table[0]))
-        totals = np.zeros(transitions.shape)
-        np.add.at(totals, ind, total_table[1])
-    else:
-        transitions = np.unique(a_trans)
-        totals = np.zeros(transitions.shape)
-
-    for transition in transitions:
-        ind = np.where(transitions == transition)
-        # Only sum values for this transition, and where soc has a valid value
-        # (negative values are missing data flags)
-        vals = a_soc[(a_trans == transition) & (a_soc > 0)]
-        totals[ind] += np.sum(vals * cell_area)
-
-    return list((transitions, totals))
-
-
-def calc_area_table(a, area_table, cell_area):
-    """Calculates an area table for an array"""
-    # Convert array to int32 dtype so that the correction below can be applied 
-    # without the array overflowing
-    a = a.astype(np.int32)
-    a_min = np.min(a)
-    if a_min < 0:
-        # Correction to add as bincount can only handle positive integers
-        correction = np.abs(a_min)
-    else:
-        correction = 0
-
-    n = np.bincount(a.ravel() + correction)
-    this_vals = np.nonzero(n)[0]
-    # Subtract correction from this_vals so area table has correct values
-    this_area_table = list([this_vals - correction, n[this_vals]])
-
-    # Don't use this_area_table if it is empty
-    if this_area_table[0].size != 0:
-        this_area_table[1] = this_area_table[1] * cell_area
-        if area_table == None:
-            area_table = this_area_table
-        else:
-            area_table = merge_area_tables(area_table, this_area_table)
-    return area_table
-
-
-def merge_area_tables(table1, table2):
-    vals = np.unique(np.concatenate([table1[0], table2[0]]))
-    count = np.zeros(vals.shape)
-
-    def add_area_table(table):
-        ind = np.concatenate(tuple(np.where(vals == item)[0] for item in table[0]))
-        np.add.at(count, ind, table[1])
-    add_area_table(table1)
-    add_area_table(table2)
-    return list((vals, count))
-
-
-# Returns value from crosstab table for particular deg/lc class combination
-def get_xtab_area(table, deg_class=None, lc_class=None):
-    deg_ind = np.where(table[0][0] == deg_class)[0]
-    lc_ind = np.where(table[0][1] == lc_class)[0]
-    if deg_ind.size != 0 and lc_ind.size != 0:
-        return float(table[1][deg_ind, lc_ind])
-    elif deg_ind.size != 0 and lc_class == None:
-        return float(np.sum(table[1][deg_ind, :]))
-    elif lc_ind.size != 0 and deg_class == None:
-        return float(np.sum(table[1][:, lc_ind]))
-    elif lc_class == None and deg_class == None:
-        return float(np.sum(table[1].ravel()))
-    else:
-        return 0
 
 
 class DlgCalculateSummaryTableAdmin(DlgCalculateBase, Ui_DlgCalculateSummaryTableAdmin):

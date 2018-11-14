3a8d7b517f2e7b1d0e821e1e9a3963b0b797ca3f
MLNoon <mnoon@conservation.org>
2018-11-14 14:50:11 -0500

    added export 9 band raster for population and urban from c04_export 5-band raster

--- gee/urban_area/src/main.py
+++ gee/urban_area/src/main.py
@@ -15,11 +15,93 @@ import ee
 from landdegradation.urban_area import urban_area
 from landdegradation.schemas.schemas import CloudResultsSchema
 
+def urban(isi_thr, ntl_thr, wat_thr, aoi_cr, EXECUTION_ID, logger):
+	#Impervious surface index computed by Trends.Earth
+	isi_series = ee.ImageCollection("projects/trends_earth/isi_20181024_esa").reduce(ee.Reducer.mean()) \
+		.select(['isi2000_mean','isi2005_mean','isi2010_mean','isi2015_mean','isi2018_mean'],
+		['isi2000','isi2005','isi2010','isi2015','isi2018'])
 
+	#JRC Global Surface Water Mapping Layers, v1.0 (>20% occurrence)
+	water = ee.Image("JRC/GSW1_0/GlobalSurfaceWater").select("occurrence")
+
+	#Gridded Population of the World Version 4, UN-Adjusted Population Density
+	gpw4_2000 = ee.Image("CIESIN/GPWv4/unwpp-adjusted-population-density/2000").select(["population-density"],["p2000"])
+	gpw4_2005 = ee.Image("CIESIN/GPWv4/unwpp-adjusted-population-density/2005").select(["population-density"],["p2005"])
+	gpw4_2010 = ee.Image("CIESIN/GPWv4/unwpp-adjusted-population-density/2010").select(["population-density"],["p2010"])
+	gpw4_2015 = ee.Image("CIESIN/GPWv4/unwpp-adjusted-population-density/2015").select(["population-density"],["p2015"])
+
+	#VIIRS Nighttime Day/Night Band Composites Version 1 (Apr 1, 2012 - May 1, 2018)
+	ntl = ee.ImageCollection("NOAA/VIIRS/DNB/MONTHLY_V1/VCMCFG").filterDate(ee.Date("2018-01-01"), ee.Date("2018-12-31")).select(["avg_rad"],["ntl"]).median()
+          
+	#DATA FORMATTING FOR EXPORT
+
+	#Mask urban areas based ntl
+	urban2000 = isi_series.select("isi2000").gte(isi_thr).unmask(0).where(ntl.lte(ntl_thr),0).multiply(10000)
+	urban2005 = isi_series.select("isi2005").gte(isi_thr).unmask(0).where(ntl.lte(ntl_thr),0).multiply(1000)
+	urban2010 = isi_series.select("isi2010").gte(isi_thr).unmask(0).where(ntl.lte(ntl_thr),0).multiply(100)
+	urban2015 = isi_series.select("isi2015").gte(isi_thr).unmask(0).where(ntl.lte(ntl_thr),0).multiply(10)
+	urban2018 = isi_series.select("isi2018").gte(isi_thr).unmask(0).where(ntl.lte(ntl_thr),0).multiply(1)
+
+	urban_series = urban2000.add(urban2005).add(urban2010).add(urban2015).add(urban2018).select(['isi2000'],['urban'])
+
+	urban_series = urban_series.where(urban_series.eq(    0),0) \
+                               .where(urban_series.eq(    1),0) \
+                               .where(urban_series.eq(   10),0) \
+                               .where(urban_series.eq(   11),4) \
+                               .where(urban_series.eq(  100),0) \
+                               .where(urban_series.eq(  101),3) \
+                               .where(urban_series.eq(  110),3) \
+                               .where(urban_series.eq(  111),3) \
+                               .where(urban_series.eq( 1000),0) \
+                               .where(urban_series.eq( 1001),2) \
+                               .where(urban_series.eq( 1010),2) \
+                               .where(urban_series.eq( 1011),2) \
+                               .where(urban_series.eq( 1100),2) \
+                               .where(urban_series.eq( 1101),2) \
+                               .where(urban_series.eq( 1110),2) \
+                               .where(urban_series.eq( 1111),2) \
+                               .where(urban_series.eq(10000),0) \
+                               .where(urban_series.eq(10001),0) \
+                               .where(urban_series.eq(10010),0) \
+                               .where(urban_series.eq(10011),1) \
+                               .where(urban_series.eq(10100),0) \
+                               .where(urban_series.eq(10101),1) \
+                               .where(urban_series.eq(10110),1) \
+                               .where(urban_series.eq(10111),1) \
+                               .where(urban_series.eq(11000),0) \
+                               .where(urban_series.eq(11001),1) \
+                               .where(urban_series.eq(11010),1) \
+                               .where(urban_series.eq(11011),1) \
+                               .where(urban_series.eq(11100),1) \
+                               .where(urban_series.eq(11101),1) \
+                               .where(urban_series.eq(11110),1) \
+                               .where(urban_series.eq(11111),1) \
+                               .where(water.gte(wat_thr),-32768)
+
+	rast_export = urban_series.addBands(gpw4_2000).addBands(gpw4_2005).addBands(gpw4_2010).addBands(gpw4_2015)
+
+	out = TEImage(rast_export,
+                  [BandInfo("Urban (2000)", add_to_map=True, metadata={'year': 2000}),
+				   BandInfo("Urban (2005)", metadata={'year': 2005}),
+				   BandInfo("Urban (2010)", metadata={'year': 2010}),
+				   BandInfo("Urban (2015)", metadata={'year': 2015}),
+				   BandInfo("Urban (2018)", add_to_map=True, metadata={'year': 2018}),
+                   BandInfo("Population (2000)", add_to_map=True, metadata={'population': 2000}),
+                   BandInfo("Population (2005)", metadata={'population': 2005}),
+				   BandInfo("Population (2010)", metadata={'population': 2010}),
+                   BandInfo("Population (2015)", add_to_map=True, metadata={'population': 2015})])
+
+    out.image = out.image.unmask(-32768).int16()
+    return out
+  
 def run(params, logger):
     """."""
     logger.debug("Loading parameters.")
-    un_adju = params.get('un_adju')
+    isi_thr = params.get('isi_thr')
+	ntl_thr = params.get('ntl_thr')
+	at_thr = params.get('at_thr')
+	aoi_cr = params.get('aoi_cr')
+
     geojsons = json.loads(params.get('geojsons'))
     crs = params.get('crs')
 
@@ -32,22 +114,10 @@ def run(params, logger):
         EXECUTION_ID = params.get('EXECUTION_ID', None)
 
     logger.debug("Running main script.")
-    outs = []
-    for geojson in geojsons:
-        this_out = None
-        urb = urban_area(geojson, un_adju, EXECUTION_ID, logger)
-        if not this_out:
-            this_out = urb
-        else:
-            this_out.merge(urb)
-        outs.append(this_out.export([geojson], 'urban_area', crs, logger, 
-                                    EXECUTION_ID, proj))
-    schema = CloudResultsSchema()
-    logger.debug("Deserializing")
-    final_output = schema.load(outs[0])
-    for o in outs[1:]:
-        this_out = schema.load(o)
-        final_output.urls.extend(this_out.urls)
-    logger.debug("Serializing")
+	 
+	out = urban(isi_thr, ntl_thr, wat_thr, aoi_cr,
+                    EXECUTION_ID, logger)
+   
     # Now serialize the output again and return it
-    return schema.dump(final_output)
+    return schema.dump(out)
+

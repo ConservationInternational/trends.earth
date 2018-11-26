cfe337f66092da3c465bdf7fc23fc1d317387758
Alex Zvoleff <azvoleff@gmail.com>
2018-11-26 14:36:39 -0500

    Convert saved type to polygon after buffering.

--- LDMP/calculate.py
+++ LDMP/calculate.py
@@ -352,6 +352,7 @@ class AOI(object):
             raise
         else:
             self.l = l_buffered
+            self.datatype = 'polygon'
 
     def isValid(self):
         return self.l.isValid()

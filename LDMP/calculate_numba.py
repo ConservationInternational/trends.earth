Fix integration tests for one step LDN calculation.

LDMP/calculate_numba.py
@@ -80,32 +80,36 @@ def make_prod5(traj, state, perf, mask):
     return(np.reshape(x, shp))
 
 
-@cc.export('ldn_total_by_trans', 'f8[:](f8[:,:], i2[:,:], i2[:], f8)')
+@cc.export('ldn_total_by_trans', 'f8[:](f8[:], i2[:], i2[:], f8)')
 def ldn_total_by_trans(a_data, a_trans, transitions, cell_area):
     """Calculates a total table for an array"""
-    a_data = a_data.ravel()
-    a_trans = a_trans.ravel()
+    # Values less than zero are missing data flags
+    a_data[a_data < 0] = 0
     totals = np.zeros(transitions.shape)
     for transition in transitions:
         # Only sum values for this transition, and where soc has a valid value
         # (negative values are missing data flags)
-        vals = a_data[(a_trans == transition) & (a_data > 0)]
-        ind = np.asarray(transitions == transition).nonzero()
-        totals[ind] += np.sum(vals * cell_area)
+        vals = a_data[a_trans == transition]
+        ind = np.asarray(transitions == transition)
+        totals[ind] = totals[ind] + np.sum(vals * cell_area)
     return totals
 
+# @cc.export('_calc_sum_f8', 'f8(f8[:,:])')
+# def _calc_sum_f8(x):
+#     s = np.sum(x)
+#     return s
 
-@cc.export('ldn_total_deg', 'f8[:](f8[:,:], b1[:,:], f8[:,:])')
+@cc.export('ldn_total_deg_f', 'f8[4](i2[:,:], b1[:,:], f8[:,:])')
 def ldn_total_deg(x, water, cell_areas):
     """Calculates a total table for an array"""
     x = x.ravel()
-    water = water.ravel()
     cell_areas = cell_areas.ravel()
-    x[water] = -32767
-    out = np.array([np.sum((x == 1) * cell_areas),
-                    np.sum((x == 0) * cell_areas),
-                    np.sum((x == -1) * cell_areas),
-                    np.sum((x == -32768) * cell_areas)])
+    x[water.ravel()] = -32767
+    out = np.zeros((4))
+    out[0] = np.sum(cell_areas[x == 1])
+    out[1] = np.sum(cell_areas[x == 0])
+    out[2] = np.sum(cell_areas[x == -1])
+    out[3] = np.sum(cell_areas[x == -32768])
     return out
 
 

LDMP/test/test_calculate_ldn.py
@@ -13,36 +13,96 @@
 """
 
 import os
+import sys
+import tempfile
+
+import numpy as np
 
 from qgis.testing import unittest
 
-from LDMP.calculate_ldn import DlgCalculateOneStep
+from LDMP.calculate_ldn import DlgCalculateLDNSummaryTableAdmin
 
 from LDMP.test import regular_keys, admin_keys, add_default_bands_to_map
 
+from LDMP.calculate_numba import ldn_make_prod5, ldn_recode_state, \
+    ldn_recode_traj, ldn_total_by_trans, ldn_total_deg_f
 
-LDN_TESTDATA = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data/Nepal_Narayani_15_3_1_TE_One_Step.tif'))
+
+LDN_TESTDATA = os.path.abspath(os.path.join(os.path.dirname(__file__), 'fixtures/Nepal_Narayani_15_3_1_TE_One_Step.tif'))
 
 
-class DegradationSummaryWorkerSDG(unittest.TestCase):
+class recode_stateTests(unittest.TestCase):
+    def recode_to_deg(self):
+        out = recode_state(np.array((-10, -2), dtype=np.int16))
+        self.assertEquals(out, np.array((-1, 1), dtype=np.int16))
+
+    def recode_to_stable(self):
+        out = recode_state(np.array((-1, 1), dtype=np.int16))
+        self.assertEquals(out, np.array((0, 0), dtype=np.int16))
+
+    def recode_to_improve(self):
+        out = recode_state(np.array((2, 10), dtype=np.int16))
+        self.assertEquals(out, np.array((1, 1), dtype=np.int16))
+
+
+class ldn_total_by_transTests(unittest.TestCase):
+    def test_zero_array(self):
+        transitions = np.array((11, 12, 33, 44), dtype=np.int16, ndmin=2)
+        total = ldn_total_by_trans(np.zeros((10, 10), dtype=np.float64),
+                                   np.zeros((10, 10), dtype=np.int16),
+                                   transitions,
+                                   12.44)
+        self.assertEquals(np.sum(total), 0.0)
+
+    # def test_zero_array_is(self):
+    #     total = ldn_total_deg_f(np.zeros((10, 10), dtype=np.int16), np.zeros((10, 10), dtype=bool), np.zeros((10, 10)))
+    #     self.assertIs(total, np.array((0., 0., 0., 0.)))
+
+
+class ldn_total_deg_fTests(unittest.TestCase):
+    def test_zero_array(self):
+        total = ldn_total_deg_f(np.zeros((10, 10), dtype=np.int16), np.zeros((10, 10), dtype=np.bool), np.zeros((10, 10), dtype=np.float64))
+        self.assertEquals(np.shape(total), (4,))
+        self.assertEquals(np.sum(total), 0.0)
+
+    # def test_zero_array_is(self):
+    #     total = ldn_total_deg_f(np.zeros((10, 10), dtype=np.int16), np.zeros((10, 10), dtype=bool), np.zeros((10, 10)))
+    #     self.assertIs(total, np.array((0., 0., 0., 0.)))
+
+
+class DlgCalculateLDNSummaryTableAdminIntegrationTests(unittest.TestCase):
     def setUp(self):
         add_default_bands_to_map(LDN_TESTDATA)
 
     def testWorker(self):
-        # deg_worker = StartWorker(DegradationSummaryWorkerSDG,
-        #                         'calculating summary table (part {} of {})'.format(n + 1, len(wkts)),
-        #                          indic_vrt,
-        #                          prod_band_nums,
-        #                          prod_mode, 
-        #                          output_sdg_tif,
-        #                          lc_band_nums, 
-        #                          soc_band_nums,
-        #                          mask_vrt)
-        #
-        self.assertEquals(0, 0)
+        d = DlgCalculateLDNSummaryTableAdmin()
+        # Need to show the summary table in order to run the setup code in that
+        # method
+        d.show()
+
+        self.out_tif_metadata = tempfile.NamedTemporaryFile(suffix='.json').name
+        self.out_tif = os.path.splitext(self.out_tif_metadata)[0] + 'tif'
+        self.out_table = tempfile.NamedTemporaryFile(suffix='.xlsx').name
+        d.output_file_layer.setText(self.out_tif_metadata)
+        d.output_file_table.setText(self.out_table)
+
+        d.area_tab.area_admin_0.setCurrentIndex(d.area_tab.area_admin_0.findText('Nepal'))
+        d.area_tab.secondLevel_area_admin_1.setCurrentIndex(d.area_tab.secondLevel_area_admin_1.findText('Narayani'))
+
+        ret = d.btn_calculate()
+        self.assertTrue(ret)
 
 
 def CalculateLDNSuite():
     suite = unittest.TestSuite()
-    suite.addTests(unittest.makeSuite(DegradationSummaryWorkerSDG, 'test'))
+    #suite.addTests(unittest.makeSuite(recode_stateTests, 'test'))
+    suite.addTests(unittest.makeSuite(ldn_total_deg_fTests, 'test'))
+    #suite.addTests(unittest.makeSuite(ldn_total_by_transTests, 'test'))
+    suite.addTests(unittest.makeSuite(DlgCalculateLDNSummaryTableAdminIntegrationTests, 'test'))
     return suite
+
+
+def run_all():
+    _suite = unittest.TestSuite()
+    _suite.addTest(CalculateLDNSuite())
+    unittest.TextTestRunner(verbosity=3, stream=sys.stdout).run(_suite)
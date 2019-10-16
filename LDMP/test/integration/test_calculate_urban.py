314ea28bd7318a772906a849eca8e65643e13826
Alex Zvoleff <azvoleff@gmail.com>
2019-10-16 15:16:49 +0200

    Update inputs.

--- LDMP/test/integration/test_calculate_urban.py
+++ LDMP/test/integration/test_calculate_urban.py
@@ -18,6 +18,8 @@ import tempfile
 
 import numpy as np
 
+import logging
+
 from qgis.testing import unittest
 
 from LDMP.calculate_urban import DlgCalculateUrbanSummaryTable
@@ -32,21 +34,23 @@ class DlgCalculateUrbanSummaryTableWorkerTests(unittest.TestCase):
     def setUp(self):
         add_default_bands_to_map(URBAN_TESTDATA)
 
-    # def testWorker(self):
-    #     d = DlgCalculateUrbanSummaryTable()
-    #     # Need to show the summary table to run the setup code in that method
-    #     d.show()
-    #     out_tif_metadata = tempfile.NamedTemporaryFile(suffix='.json').name
-    #     out_tif = os.path.splitext(out_tif_metadata)[0] + 'tif'
-    #     out_table = tempfile.NamedTemporaryFile(suffix='.xlsx').name
-    #     d.output_file_layer.setText(out_tif_metadata)
-    #     d.output_file_table.setText(out_table)
-    #     d.area_tab.area_admin_0.setCurrentIndex(d.area_tab.area_admin_0.findText('Nepal'))
-    #     d.area_tab.secondLevel_area_admin_1.setCurrentIndex(d.area_tab.secondLevel_area_admin_1.findText('Narayani'))
-    #
-    #     ret = d.btn_calculate()
-    #     self.assertTrue(ret)
-    #     
+    def testWorker(self):
+        d = DlgCalculateUrbanSummaryTable()
+        # Need to show the summary table to run the setup code in that method
+        d.show()
+        out_tif_metadata = tempfile.NamedTemporaryFile(suffix='.json').name
+        out_tif = os.path.splitext(out_tif_metadata)[0] + 'tif'
+        out_table = tempfile.NamedTemporaryFile(suffix='.xlsx').name
+        d.output_file_layer.setText(out_tif_metadata)
+        d.output_file_table.setText(out_table)
+        d.area_tab.area_admin_0.setCurrentIndex(d.area_tab.area_admin_0.findText('Suriname'))
+        d.area_tab.radioButton_secondLevel_city.setChecked(True)
+        d.area_tab.groupBox_buffer.setChecked(True)
+        d.area_tab.secondLevel_city.setCurrentIndex(d.area_tab.secondLevel_city.findText('Paramaribo (Paramaribo)'))
+        d.area_tab.buffer_size_km.setValue(10)
+
+        ret = d.btn_calculate()
+        self.assertTrue(ret)
 
     def test_table(self):
         self.assertTrue(True)

203621fffd9982a4c0a17e0626fd32a218588c10
Alex Zvoleff <azvoleff@gmail.com>
2020-04-27 13:23:05 -0400

    Add major version.

--- LDMP/__init__.py
+++ LDMP/__init__.py
@@ -14,7 +14,7 @@
 
 import sys
 import os
-import requests
+import re
 import site
 import json
 from tempfile import NamedTemporaryFile
@@ -30,6 +30,7 @@ plugin_dir = os.path.dirname(os.path.realpath(__file__))
 with open(os.path.join(plugin_dir, 'version.json')) as f:
     version_info = json.load(f)
 __version__ = version_info['version']
+__version_major__ = re.sub(r'([0-9]+)(\.[0-9]+)+$', r'\g<1>', __version__)
 __revision__ = version_info['revision']
 __release_date__ = version_info['release_date']
 

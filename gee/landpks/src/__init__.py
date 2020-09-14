bcdecd84b69f74ddd9ec28852d73af84cfdc0253
Alex Zvoleff <azvoleff@conservation.org>
2020-09-14 13:46:42 -0400

    Add LandPKS script.

---
+++ gee/landpks/src/__init__.py
@@ -0,0 +1,15 @@
+__version__ =  "1.0.3"
+
+import sys
+import os
+import rollbar
+
+from gefcore import logger
+
+rollbar.init(os.getenv('ROLLBAR_SCRIPT_TOKEN'), os.getenv('ENV'))
+
+def rollbar_except_hook(exc_type, exc_value, traceback):
+   # Report the issue to rollbar here.
+   rollbar.report_exc_info((exc_type, exc_value, traceback))
+   # display the error as normal here
+   sys.__excepthook__(exc_type, exc_value, traceback)

04e55530a70c060f7d2752b7451345f0b0851414
Luigi Pirelli <luipir@gmail.com>
2021-03-19 16:53:22 +0100

    MVC implemented. Fixes #367 (#382)
    Implement algorithms tab of the plugin dock
    
    Added model for managing algorithms. This is set up with a tree view in the GUI. Some additional code to manage showing a custom widget with a delegate and also having it be in persisting edit state to improve UX.

--- LDMP/main_widget.py
+++ LDMP/main_widget.py
@@ -18,6 +18,14 @@ from qgis.core import QgsSettings
 from LDMP import __version__, log
 from LDMP.message_bar import MessageBar
 from LDMP.gui.WidgetMain import Ui_dockWidget_trends_earth
+from LDMP.models.algorithms import (
+    AlgorithmGroup,
+    AlgorithmDescriptor,
+    AlgorithmRunMode,
+    AlgorithmDetails
+)
+from LDMP.models.algorithms_model import AlgorithmTreeModel
+from LDMP.models.algorithms_delegate import AlgorithmItemDelegate
 
 settings = QgsSettings()
 
@@ -36,6 +44,73 @@ class MainWidget(QtWidgets.QDockWidget, Ui_dockWidget_trends_earth):
         super(MainWidget, self).__init__(parent)
 
         self.setupUi(self)
+        self.setupAlgorithmsTree()
+
+    def setupAlgorithmsTree(self):
+        # setup algorithms and their hierarchy
+        tree = AlgorithmGroup(name='root', name_details='root details', parent=None, algorithms=[])
+        first = AlgorithmGroup(name='Proportion of land that is degradated over total land area', name_details='SDG 15.3.1', parent=tree, algorithms=[])
+        second = AlgorithmGroup(name='Ratio of land consumption rate to population growth rate', name_details='SDG 11.3.1', parent=tree, algorithms=[])
+        third = AlgorithmGroup(name='Miscellaneous', name_details=None, parent=tree, algorithms=[])
+
+        alg1G1 = AlgorithmDescriptor(name='Land productivity', name_details='sub-indicator 1', brief_description=None, details=None, parent=first)
+        alg1G1.run_mode = AlgorithmRunMode.Both
+        alg1G1_details = AlgorithmDetails(name=None, name_details=None, description='alg1G1 long description', parent=alg1G1)
+        alg1G1.setDetails(alg1G1_details)
+
+        alg2G1 = AlgorithmDescriptor(name='Land productivity based on JRC LDP', name_details='sub-indicator 1', brief_description=None, details=None, parent=first)
+        alg2G1.run_mode = AlgorithmRunMode.Locally
+        alg2G1_details = AlgorithmDetails(name=None, name_details=None, description='alg2G1 long description', parent=alg2G1)
+        alg2G1.setDetails(alg2G1_details)
+
+        alg3G1 = AlgorithmDescriptor(name='Land change', name_details='sub-indicator 2', brief_description=None, details=None, parent=first)
+        alg3G1.run_mode = AlgorithmRunMode.Remotely
+        # no details here!
+
+        alg4G1 = AlgorithmDescriptor(name='Carbon soil', name_details='sub-indicator 3', brief_description=None, details=None, parent=first)
+        alg4G1.run_mode = AlgorithmRunMode.NotApplicable
+        alg4G1_details = AlgorithmDetails(name=None, name_details=None, description='alg4G1 long description', parent=alg4G1)
+        alg4G1.setDetails(alg4G1_details)
+
+        alg5G1 = AlgorithmDescriptor(name='Land degradation neutrality', name_details='main SDG 15.3.1 indicator', brief_description=None, details=None, parent=first)
+        alg5G1.run_mode = AlgorithmRunMode.Both
+        alg5G1_details = AlgorithmDetails(name=None, name_details=None, description='This is the main dindicator that bla bla bla bla bla', parent=alg5G1)
+        alg5G1.setDetails(alg5G1_details)
+
+        first.algorithms.append(alg1G1)
+        first.algorithms.append(alg2G1)
+        first.algorithms.append(alg3G1)
+        first.algorithms.append(alg4G1)
+        first.algorithms.append(alg5G1)
+
+        alg1G2 = AlgorithmDescriptor(name='alg1G2', name_details='alg1G2 details', brief_description=None, details=None, parent=second)
+        alg1G2.run_mode = AlgorithmRunMode.Both
+        alg1G2_details = AlgorithmDetails(name=None, name_details=None, description='This is the second dindicator that bla bla bla bla bla', parent=alg1G2)
+        alg1G2.setDetails(alg1G2_details)
+        second.algorithms.append(alg1G2)
+
+        alg1G3 = AlgorithmDescriptor(name='alg1G3', name_details='alg1G3 details', brief_description=None, details=None, parent=third)
+        alg1G3.run_mode = AlgorithmRunMode.NotApplicable
+        alg1G3_details = AlgorithmDetails(name=None, name_details=None, description='This is description for miscellaneous stuffs', parent=alg1G3)
+        alg1G3.setDetails(alg1G3_details)
+        third.algorithms.append(alg1G3)
+
+
+        tree.algorithms.append(first)
+        tree.algorithms.append(second)
+        tree.algorithms.append(third)
+
+        # show it
+        algorithmsModel = AlgorithmTreeModel(tree)
+        self.treeView_algorithms.setMouseTracking(True) # to allow emit entered events and manage editing over mouse
+        self.treeView_algorithms.setWordWrap(True) # add ... to wrap DisplayRole text... to have a real wrap need a custom widget
+        self.treeView_algorithms.setModel(algorithmsModel)
+        delegate = AlgorithmItemDelegate(self.treeView_algorithms)
+        self.treeView_algorithms.setItemDelegate(delegate)
+
+        # configure View how to enter editing mode
+        self.treeView_algorithms.setEditTriggers(QtWidgets.QAbstractItemView.AllEditTriggers)
+
 
     def closeEvent(self, event):
         super(MainWidget, self).closeEvent(event)

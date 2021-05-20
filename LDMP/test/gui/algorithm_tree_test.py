661ff714efcef18f154a1b6d6628a67f43363a5a
Luigi Pirelli <luipir@gmail.com>
2021-05-20 16:53:48 +0200

    updated test to check stylesheet options

--- LDMP/test/gui/algorithm_tree_test.py
+++ LDMP/test/gui/algorithm_tree_test.py
@@ -82,9 +82,10 @@ class Test(object):
         experimental = AlgorithmGroup(name=tr('Experimental'), name_details=None, parent=tree, algorithms=[])
         total_carbon_group = AlgorithmGroup(name=tr('Total carbon'), name_details=tr('above and belowground, emissions and deforestation'), parent=experimental, algorithms=[])
         potential_change_group = AlgorithmGroup(name=tr('Potential change in biomass due to restoration'), name_details=tr('above and belowground woody'), parent=experimental, algorithms=[])
+
         # land_degradation_group
         land_productivity_alg = AlgorithmDescriptor(
-            name=tr('Land productivity'), 
+            name=tr('Land productivity'),
             name_details=None,
             brief_description=None,
             details=None,
@@ -97,6 +98,7 @@ class Test(object):
             description=tr('TODO: Land productivity long description'),
             parent=land_productivity_alg)
         land_productivity_alg.setDetails(land_productivity_alg_details)
+
         land_cover_alg = AlgorithmDescriptor(
             name=tr('Land cover'),
             name_details=None,
@@ -111,6 +113,7 @@ class Test(object):
             description=tr('TODO: Land cover long description'),
             parent=land_cover_alg)
         land_cover_alg.setDetails(land_cover_alg_details)
+
         soil_organic_carbon_alg = AlgorithmDescriptor(
             name=tr('Soil organic carbon'),
             name_details=None,
@@ -125,6 +128,7 @@ class Test(object):
             description=tr('TODO: Soil organic carbon long description'),
             parent=soil_organic_carbon_alg)
         soil_organic_carbon_alg.setDetails(soil_organic_carbon_alg_details)
+
         all_land_degradation_alg = AlgorithmDescriptor(
             name=tr('All above sub-indicators in one step'),
             name_details=None,
@@ -139,6 +143,7 @@ class Test(object):
             description=tr('TODO: All abovesub-indicators in one step long description'),
             parent=all_land_degradation_alg)
         all_land_degradation_alg.setDetails(all_land_degradation_alg_details)
+
         final_alg = AlgorithmDescriptor(
             name=tr('Final SDG 15.3.1'),
             name_details=tr('Spatial layer and summary table for total boundary'),
@@ -153,6 +158,7 @@ class Test(object):
             description=tr('TODO: Final SDG 15.3.1 long description'),
             parent=final_alg)
         final_alg.setDetails(final_alg_details)
+
         area_summaries_alg = AlgorithmDescriptor(
             name=tr('Area summaries of a raster on sub-units'),
             name_details=None,
@@ -167,12 +173,14 @@ class Test(object):
             description=tr('TODO: Area summaries long description'),
             parent=area_summaries_alg)
         area_summaries_alg.setDetails(area_summaries_alg_details)
+
         land_degradation_group.algorithms.append(land_productivity_alg)
         land_degradation_group.algorithms.append(land_cover_alg)
         land_degradation_group.algorithms.append(soil_organic_carbon_alg)
         land_degradation_group.algorithms.append(all_land_degradation_alg)
         land_degradation_group.algorithms.append(final_alg)
         land_degradation_group.algorithms.append(area_summaries_alg)
+
         # urban_change_group
         urban_change_alg = AlgorithmDescriptor(
             name=tr('Urban change spatial layer'),
@@ -188,6 +196,7 @@ class Test(object):
             description=tr('TODO: Urban change spatial layer long description'),
             parent=urban_change_alg)
         urban_change_alg.setDetails(urban_change_alg_details)
+
         urban_change_summary_alg = AlgorithmDescriptor(
             name=tr('Urban change summary table for city'),
             name_details=None,
@@ -202,8 +211,10 @@ class Test(object):
             description=tr('TODO: Urban change summary table for city long description'),
             parent=urban_change_summary_alg)
         urban_change_summary_alg.setDetails(urban_change_summary_alg_details)
+
         urban_change_group.algorithms.append(urban_change_alg)
         urban_change_group.algorithms.append(urban_change_summary_alg)
+
         # total_carbon_group
         carbon_change_alg = AlgorithmDescriptor(
             name=tr('Carbon change spatial layers'),
@@ -219,6 +230,7 @@ class Test(object):
             description=tr('TODO: Carbon change spatial layers long description'),
             parent=carbon_change_alg)
         carbon_change_alg.setDetails(carbon_change_alg_details)
+
         carbon_change_summary_alg = AlgorithmDescriptor(
             name=tr('Carbon change summary table for boundary'),
             name_details=None,
@@ -229,9 +241,11 @@ class Test(object):
         carbon_change_summary_alg.run_mode = AlgorithmRunMode.Remotely
         carbon_change_summary_alg_details = AlgorithmDetails(name=None, name_details=None, description=tr('TODO: Carbon change summary table for boundary long description'), parent=carbon_change_summary_alg)
         carbon_change_summary_alg.setDetails(carbon_change_summary_alg_details)
+
         total_carbon_group.algorithms.append(carbon_change_alg)
         total_carbon_group.algorithms.append(carbon_change_summary_alg)
         experimental.algorithms.append(total_carbon_group)
+
         # potential_change_group
         estimate_biomass_change_alg = AlgorithmDescriptor(
             name=tr('Estimate biomass change'),
@@ -247,6 +261,7 @@ class Test(object):
             description=tr('TODO: Estimate biomass change long description'),
             parent=estimate_biomass_change_alg)
         estimate_biomass_change_alg.setDetails(estimate_biomass_change_alg_details)
+
         estimate_biomass_summary_alg = AlgorithmDescriptor(
             name=tr('Table summarizing likely changes in biomass'),
             name_details=None,
@@ -257,21 +272,23 @@ class Test(object):
         estimate_biomass_summary_alg.run_mode = AlgorithmRunMode.Remotely
         estimate_biomass_summary_alg_details = AlgorithmDetails(name=None, name_details=None, description=tr('TODO: Table summarizing likely changes in biomass long description'), parent=estimate_biomass_summary_alg)
         estimate_biomass_summary_alg.setDetails(estimate_biomass_summary_alg_details)
+
         potential_change_group.algorithms.append(estimate_biomass_change_alg)
         potential_change_group.algorithms.append(estimate_biomass_summary_alg)
         experimental.algorithms.append(potential_change_group)
+
         # populate tree
         tree.algorithms.append(land_degradation_group)
         tree.algorithms.append(urban_change_group)
         tree.algorithms.append(experimental)
 
-        app = QApplication(["???"])
-
         # show it
+        app = QApplication(["???"])
         algorithmsModel = AlgorithmTreeModel(tree)
         QAbstractItemModelTester(algorithmsModel, QAbstractItemModelTester.FailureReportingMode.Warning)
 
         view = QTreeView()
+        view.setStyleSheet('QTreeView { selection-background-color: white }')
         view.setMouseTracking(True) # to allow emit entered events and manage editing over mouse
         view.setWordWrap(True)
         view.setEditTriggers(QAbstractItemView.AllEditTriggers)

fe71e978463482adda4d96020c5b8217350bcc38
Samweli <samweli@kartoza.com>
2021-06-08 09:03:37 +0300

    updated land cover algorithm dialog

--- LDMP/calculate_lc.py
+++ LDMP/calculate_lc.py
@@ -21,7 +21,13 @@ from PyQt5 import (
     QtWidgets,
     uic
 )
-import qgis.gui
+from builtins import zip
+from builtins import range
+from datetime import datetime
+
+from qgis.PyQt import QtWidgets
+from qgis.PyQt.QtCore import QSettings, Qt
+
 import qgis.core
 from qgis.utils import iface
 
@@ -127,30 +133,41 @@ class DlgCalculateLC(calculate.DlgCalculateBase, DlgCalculateLcUi):
         super().__init__(iface, script, parent)
         self.setupUi(self)
 
+        self.lc_setup_widget = LCSetupWidget()
+        self.lc_define_deg_widget = LCDefineDegradationWidget
+
+        self.initiliaze_settings()
+
     def showEvent(self, event):
         super().showEvent(event)
 
-        self.lc_setup_tab = LCSetupWidget()
-        self.TabBox.insertTab(0, self.lc_setup_tab, self.tr('Land Cover Setup'))
-
-        self.lc_define_deg_tab = LCDefineDegradationWidget()
-        self.TabBox.insertTab(1, self.lc_define_deg_tab, self.tr('Define Degradation'))
         # These boxes may have been hidden if this widget was last shown on the 
         # SDG one step dialog
-        self.lc_setup_tab.groupBox_esa_period.show()
-        self.lc_setup_tab.use_custom.show()
-        self.lc_setup_tab.groupBox_custom_bl.show()
-        self.lc_setup_tab.groupBox_custom_tg.show()
+        self.lc_setup_widget.groupBox_esa_period.show()
+        self.lc_setup_widget.use_custom.show()
+        self.lc_setup_widget.groupBox_custom_bl.show()
+        self.lc_setup_widget.groupBox_custom_tg.show()
 
-        # This box may have been hidden if this widget was last shown on the 
-        # SDG one step dialog
-        self.lc_setup_tab.groupBox_esa_period.show()
+    def showEvent(self, event):
+        super(DlgCalculateLC, self).showEvent(event)
 
-        if self.reset_tab_on_showEvent:
-            self.TabBox.setCurrentIndex(0)
+        if self.setup_frame.layout() is None:
+            setup_layout = QtWidgets.QVBoxLayout(self.setup_frame)
+            setup_layout.setContentsMargins(0, 0, 0, 0)
+            setup_layout.addWidget(self.lc_setup_widget)
+            self.setup_frame.setLayout(setup_layout)
 
-        self.lc_setup_tab.use_custom_initial.populate()
-        self.lc_setup_tab.use_custom_final.populate()
+            scroll_container = QtWidgets.QWidget()
+            layout = QtWidgets.QVBoxLayout()
+            layout.setContentsMargins(1, 1, 1, 1)
+            layout.setSpacing(1)
+            layout.addWidget(self.lc_define_deg_widget)
+            scroll_container.setLayout(layout)
+            scroll_container.setLayout(layout)
+            self.scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+            self.scroll_area.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+            self.scroll_area.setWidgetResizable(True)
+            self.scroll_area.setWidget(scroll_container)
 
     def btn_calculate(self):
         # Note that the super class has several tests in it - if they fail it
@@ -160,26 +177,79 @@ class DlgCalculateLC(calculate.DlgCalculateBase, DlgCalculateLcUi):
         if not ret:
             return
 
-        if self.lc_setup_tab.use_esa.isChecked():
+        if self.lc_setup_widget.use_esa.isChecked():
             self.calculate_on_GEE()
         else:
             self.calculate_locally()
 
+    def splitter_toggled(self):
+        if self.splitter_collapsed:
+            self.splitter.restoreState(self.splitter_state)
+            self.collapse_button.setArrowType(Qt.RightArrow)
+        else:
+            self.splitter_state = self.splitter.saveState()
+            self.splitter.setSizes([1, 0])
+            self.collapse_button.setArrowType(Qt.LeftArrow)
+        self.splitter_collapsed = not self.splitter_collapsed
+
+    def initiliaze_settings(self):
+
+        ok_button = self.button_box.button(
+            QtWidgets.QDialogButtonBox.Ok
+        )
+        ok_button.setText(self.tr("Schedule execution"))
+        ok_button.clicked.connect(self.btn_calculate)
+
+        region = QgsSettings().value("trends_earth/region_of_interest/area_settings_name")
+        self.execution_name_le.setText(f"LDN_{region}_{datetime.strftime(datetime.now(), '%Y%m%d%H%M%S')}")
+
+        self.region_button.clicked.connect(self.run_settings)
+
+        # adding a collapsible arrow on the splitter
+        self.splitter_state = self.splitter.saveState()
+        self.splitter.setCollapsible(0, False)
+        splitter_handle = self.splitter.handle(1)
+        handle_layout = QtWidgets.QVBoxLayout()
+        handle_layout.setContentsMargins(0, 0, 0, 0)
+        self.collapse_button = QtWidgets.QToolButton(splitter_handle)
+        self.collapse_button.setAutoRaise(True)
+        self.collapse_button.setFixedSize(12, 12)
+        self.collapse_button.setCursor(Qt.ArrowCursor)
+        handle_layout.addWidget(self.collapse_button)
+
+        handle_layout.addStretch()
+        splitter_handle.setLayout(handle_layout)
+
+        arrow_type = Qt.RightArrow if self.splitter.sizes()[1] == 0 else Qt.LeftArrow
+
+        self.collapse_button.setArrowType(arrow_type)
+        self.collapse_button.clicked.connect(self.splitter_toggled)
+        self.splitter_collapsed = self.splitter.sizes()[1] != 0
+
+        QgsGui.enableAutoGeometryRestore(self)
+
+        self.region_la.setText(self.tr(f"Current region: {region}"))
+
+    def run_settings(self):
+        dlg_settings = DlgSettings()
+        dlg_settings.show()
+        result = dlg_settings.exec_()
+
     def calculate_on_GEE(self):
         self.close()
         crosses_180th, geojsons = self.gee_bounding_box
-        payload = {
-            'year_baseline': self.lc_setup_tab.use_esa_bl_year.date().year(),
-            'year_target': self.lc_setup_tab.use_esa_tg_year.date().year(),
-            'geojsons': json.dumps(geojsons),
-            'crs': self.aoi.get_crs_dst_wkt(),
-            'crosses_180th': crosses_180th,
-            'trans_matrix': self.lc_define_deg_tab.trans_matrix_get(),
-            'remap_matrix': self.lc_setup_tab.dlg_esa_agg.get_agg_as_list(),
-            'task_name': self.options_tab.task_name.text(),
-            'task_notes': self.options_tab.task_notes.toPlainText()
-        }
+        payload = {'year_baseline': self.lc_setup_widget.use_esa_bl_year.date().year(),
+                   'year_target': self.lc_setup_widget.use_esa_tg_year.date().year(),
+                   'geojsons': json.dumps(geojsons),
+                   'crs': self.aoi.get_crs_dst_wkt(),
+                   'crosses_180th': crosses_180th,
+                   'trans_matrix': self.lc_define_deg_widget.trans_matrix_get(),
+                   'remap_matrix': self.lc_setup_widget.dlg_esa_agg.get_agg_as_list(),
+                   'task_name': self.execution_name_le.text(),
+                   'task_notes': self.task_notes.toPlainText()}
+
         resp = job_manager.submit_remote_job(payload, self.script.id)
+
         if resp:
             main_msg = "Submitted"
             description = "Land cover task submitted to Google Earth Engine."
@@ -194,7 +264,7 @@ class DlgCalculateLC(calculate.DlgCalculateBase, DlgCalculateLcUi):
         )
 
     def calculate_locally(self):
-        if len(self.lc_setup_tab.use_custom_initial.layer_list) == 0:
+        if len(self.lc_setup_widget.use_custom_initial.layer_list) == 0:
             QtWidgets.QMessageBox.critical(
                 None,
                 self.tr("Error"),
@@ -205,7 +275,7 @@ class DlgCalculateLC(calculate.DlgCalculateBase, DlgCalculateLcUi):
             )
             return
 
-        if len(self.lc_setup_tab.use_custom_final.layer_list) == 0:
+        if len(self.lc_setup_widget.use_custom_final.layer_list) == 0:
             QtWidgets.QMessageBox.critical(
                 None,
                 self.tr("Error"),
@@ -216,9 +286,8 @@ class DlgCalculateLC(calculate.DlgCalculateBase, DlgCalculateLcUi):
             )
             return
 
-
-        year_baseline = self.lc_setup_tab.get_initial_year()
-        year_target = self.lc_setup_tab.get_final_year()
+        year_baseline = self.get_initial_year()
+        year_target = self.get_final_year()
         if int(year_baseline) >= int(year_target):
             QtWidgets.QMessageBox.information(
                 None,
@@ -230,7 +299,7 @@ class DlgCalculateLC(calculate.DlgCalculateBase, DlgCalculateLcUi):
                 )
             )
 
-        initial_layer = self.lc_setup_tab.use_custom_initial.get_layer()
+        initial_layer = self.lc_setup_widget.use_custom_initial.get_layer()
         initial_extent_geom = qgis.core.QgsGeometry.fromRect(initial_layer.extent())
         if self.aoi.calc_frac_overlap(initial_extent_geom) < .99:
             QtWidgets.QMessageBox.critical(
@@ -243,7 +312,7 @@ class DlgCalculateLC(calculate.DlgCalculateBase, DlgCalculateLcUi):
             )
             return
 
-        final_layer = self.lc_setup_tab.use_custom_final.get_layer()
+        final_layer = self.lc_setup_widget.use_custom_final.get_layer()
         final_extent_geom = qgis.core.QgsGeometry.fromRect(final_layer.extent())
         if self.aoi.calc_frac_overlap(final_extent_geom) < .99:
             QtWidgets.QMessageBox.critical(
@@ -258,11 +327,11 @@ class DlgCalculateLC(calculate.DlgCalculateBase, DlgCalculateLcUi):
 
         self.close()
 
-        initial_usable = self.lc_setup_tab.use_custom_initial.get_usable_band_info()
-        final_usable = self.lc_setup_tab.use_custom_final.get_usable_band_info()
+        initial_usable = self.lc_setup_widget.use_custom_initial.get_usable_band_info()
+        final_usable = self.lc_setup_widget.use_custom_final.get_usable_band_info()
         job_params = {
-            "task_name": self.options_tab.task_name.text(),
-            "task_notes": self.options_tab.task_notes.toPlainText(),
+            "task_name": self.execution_name_le.text(),
+            "task_notes": self.task_notes.toPlainText(),
             "year_baseline": year_baseline,
             "year_target": year_target,
             "lc_initial_path": str(initial_usable.path),

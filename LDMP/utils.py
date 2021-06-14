92ffc3bd6b529210872f153d62b31e09e6201f07
Ricardo Garcia Silva <ricardo@kartoza.com>
2021-06-14 14:50:22 +0100

    Add missing files from last commit and add some extra stuff to .gitignore

---
+++ LDMP/utils.py
@@ -0,0 +1,8 @@
+import importlib
+import typing
+
+
+def load_object(python_path: str) -> typing.Any:
+    module_path, object_name = python_path.rpartition(".")[::2]
+    loaded_module = importlib.import_module(module_path)
+    return getattr(loaded_module, object_name)

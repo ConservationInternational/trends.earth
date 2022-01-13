cf2795cacfb63bb3902f4b1dcd2f464249fc8d8e
Alex Zvoleff <azvoleff@conservation.org>
2022-01-12 23:35:00 -0500

    Fix to use latest schema

--- gee/sdg-15-3-1-sub-indicators/src/main.py
+++ gee/sdg-15-3-1-sub-indicators/src/main.py
@@ -175,7 +175,7 @@ def run_te_for_period(params, max_workers, EXECUTION_ID, logger):
             )
         )
 
-    schema = results.CloudResultsV2.Schema()
+    schema = results.RasterResults.Schema()
     logger.debug("Deserializing - setting up main output")
     logger.debug(f"outs[0].keys() {outs[0].keys()}")
     final_output = schema.load(outs[0])

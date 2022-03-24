5ca621c4c485cac3f10bc943e669cdf55f59d138
John G <gkahiu@gmail.com>
2022-03-24 12:52:52 +0300

    Update with latest version number.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,47 +10,68 @@
 #
 # All configuration values have a default; values that are commented out
 # serve to show the default.
-
-import sys
 import os
-import sphinx_rtd_theme
+import sys
 from datetime import date
+
+import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+# sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+# needs_sphinx = '1.0'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-#extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'rinoh.frontend.sphinx']
-#extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'rst2pdf.pdfbuilder']
-extensions = ['sphinx_rtd_theme','sphinx.ext.todo', 'sphinx.ext.viewcode']
+# extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'rinoh.frontend.sphinx']
+# extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'rst2pdf.pdfbuilder']
+extensions = [
+    "sphinx_rtd_theme",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "myst_parser",
+]
 
-todo_include_todos=True
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
+
+todo_include_todos = True
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix of source filenames.
-source_suffix = '.rst'
+source_suffix = [".rst", ".md"]
 
 # The encoding of source files.
-#source_encoding = 'utf-8-sig'
+# source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
-project = u'Trends.Earth'
-copyright = u'2017-{}, Conservation International'.format(date.today().year)
+project = u"Trends.Earth"
+copyright = u"2017-{}, Conservation International".format(date.today().year)
 
-locale_dirs = ['../i18n/']
+locale_dirs = ["../i18n/"]
 gettext_compact = False
 
 # The version info for the project you're documenting, acts as replacement for
@@ -58,96 +79,98 @@ gettext_compact = False
 # built documents.
 #
 # The short X.Y version.
-version = '1.99.1'
+version = "1.99.7"
 # The full version, including alpha/beta/rc tags.
-release = '1.99.1'
+release = "1.99.7"
 
 rst_epilog = """
-.. |iconCalculator| image:: ../../resources/en/common/icon-calculator.png
+.. |iconCalculator| image:: /static/common/icon-calculator.png
    :width: 2em
-.. |iconChart| image:: ../../resources/en/common/icon-chart.png
+.. |iconChart| image:: /static/common/icon-chart.png
    :width: 2em
-.. |iconClipboard| image:: ../../resources/en/common/icon-clipboard.png
+.. |iconClipboard| image:: /static/common/icon-clipboard.png
    :width: 2em
-.. |iconCloudDownload| image:: ../../resources/en/common/icon-cloud-download.png
+.. |iconCloudDownload| image:: /static/common/icon-cloud-download.png
    :width: 2em
-.. |iconCog| image:: ../../resources/en/common/icon-cog.png
+.. |iconCog| image:: /static/common/icon-cog.png
    :width: 2em
-.. |iconDeletedataset| image:: ../../resources/en/common/icon-delete_dataset.png
+.. |iconDeletedataset| image:: /static/common/icon-delete_dataset.png
    :width: 2em
-.. |iconGlobe| image:: ../../resources/en/common/icon-globe.png
+.. |iconGlobe| image:: /static/common/icon-globe.png
    :width: 2em
-.. |iconGraph| image:: ../../resources/en/common/icon-graph.png
+.. |iconGraph| image:: /static/common/icon-graph.png
    :width: 2em
-.. |iconInfo| image:: ../../resources/en/common/icon-info.png
+.. |iconInfo| image:: /static/common/icon-info.png
    :width: 2em
-.. |iconLoaddataset| image:: ../../resources/en/common/icon-load_dataset_onto_qgis_map_area.png
-.. |iconMarkMarker| image:: ../../resources/en/common/icon-map-marker.png
+.. |iconLoaddataset| image:: /static/common/icon-load_dataset_onto_qgis_map_area.png
+.. |iconMarkMarker| image:: /static/common/icon-map-marker.png
    :width: 2em
-.. |iconOpendatasetdirectory| image:: ../../resources/en/common/icon-open_dataset_directory.png
+.. |iconOpendatasetdirectory| image:: /static/common/icon-open_dataset_directory.png
    :width: 2em
-.. |iconViewdatasetdetails| image:: ../../resources/en/common/icon-view_dataset_details.png
+.. |iconViewdatasetdetails| image:: /static/common/icon-view_dataset_details.png
    :width: 2em
-.. |iconWrench| image:: ../../resources/en/common/icon-wrench.png
+.. |iconWrench| image:: /static/common/icon-wrench.png
    :width: 2em
-.. |iconFolder| image:: ../../resources/en/common/icon-folder.png
+.. |iconFolder| image:: /static/common/icon-folder.png
    :width: 2em
-.. |iconTrendsEarth| image:: ../../resources/en/common/icon-trends_earth.png
+.. |iconTrendsEarth| image:: /static/common/icon-trends_earth.png
    :width: 2em
-.. |iconVisualization| image:: ../../resources/en/common/icon-reporting.png
+.. |iconVisualization| image:: /static/common/icon-reporting.png
    :width: 2em
-.. |trends.earth| image:: ../../resources/en/common/trends_earth_logo_bl_print.png
+.. |trends.earth| image:: /static/common/trends_earth_logo_bl_print.png
    :width: 7em
    :alt: Trends.Earth
-.. |logoCI| image:: ../../resources/en/common/logo_CI_square.png
+.. |logoCI| image:: /static/common/logo_CI_square.png
     :width: 150
     :target: http://www.conservation.org
-.. |logoLund| image:: ../../resources/en/common/logo_Lund_square.png
+.. |logoLund| image:: /static/common/logo_Lund_square.png
     :width: 125
     :target: http://www.lunduniversity.lu.se
-.. |logoNASA| image:: ../../resources/en/common/logo_NASA_square.png
+.. |logoNASA| image:: /static/common/logo_NASA_square.png
     :width: 125
     :target: http://www.nasa.gov
-.. |logoGEF| image:: ../../resources/en/common/logo_GEF.png
+.. |logoGEF| image:: /static/common/logo_GEF.png
     :width: 125
     :target: https://www.thegef.org
 .. |CURRENT| replace:: {current_version}
-.. |qgisMinVersion| replace:: 3.10.3
-""".format(current_version=version)
+.. |qgisMinVersion| replace:: 3.14
+""".format(
+    current_version=version
+)
 
 # The language for content autogenerated by Sphinx. Refer to documentation
 # for a list of supported languages.
-language = 'en'
+language = "en"
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
-#today = ''
+# today = ''
 # Else, today_fmt is used as the format for a strftime call.
-#today_fmt = '%B %d, %Y'
+# today_fmt = '%B %d, %Y'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 exclude_patterns = ["../resources"]
 
 # The reST default role (used for this markup: `text`) to use for all documents.
-#default_role = None
+# default_role = None
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
-#add_function_parentheses = True
+# add_function_parentheses = True
 
 # If true, the current module name will be prepended to all description
 # unit titles (such as .. function::).
-#add_TemplateModuleNames = True
+# add_TemplateModuleNames = True
 
 # If true, sectionauthor and moduleauthor directives will be shown in the
 # output. They are ignored by default.
-#show_authors = False
+# show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # A list of ignored prefixes for module index sorting.
-#modindex_common_prefix = []
+# modindex_common_prefix = []
 
 # -- Options for HTML output ---------------------------------------------------
 
@@ -158,128 +181,122 @@ html_theme = "sphinx_rtd_theme"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+# html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+# html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+# html_short_title = None
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = '../resources/en/common/trends_earth_logo_bl_1200.png'
-html_logo = '../resources/en/common/trends_earth_logo_square_32x32.ico'
+html_logo = "trends_earth_logo_square_32x32.ico"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = '../resources/en/common/trends_earth_logo_square_32x32.ico'
+html_favicon = "trends_earth_logo_square_32x32.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['static']
+html_static_path = ["static"]
 
 html_show_sourcelink = False
 
 # Note the underscore SHOULD be used below as this is how the static folder is
 # named by sphinx on generation.
 html_context = {
-    'css_files': ['_static/custom.css'],
+    "css_files": ["_static/custom.css"],
 }
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-#html_last_updated_fmt = '%b %d, %Y'
+# html_last_updated_fmt = '%b %d, %Y'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
-#html_use_smartypants = True
+# html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
+# html_sidebars = {}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
-#html_additional_pages = {}
+# html_additional_pages = {}
 
 # If false, no module index is generated.
-#html_domain_indices = True
+# html_domain_indices = True
 
 # If false, no index is generated.
-#html_use_index = True
+# html_use_index = True
 
 # If true, the index is split into individual pages for each letter.
-#html_split_index = False
+# html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
+# html_show_sourcelink = True
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-#html_show_sphinx = True
+# html_show_sphinx = True
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
+# html_show_copyright = True
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
 # base URL from which the finished HTML is served.
-#html_use_opensearch = ''
+# html_use_opensearch = ''
 
 # This is the file name suffix for HTML files (e.g. ".xhtml").
-#html_file_suffix = None
+# html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'TemplateClassdoc'
+htmlhelp_basename = "TemplateClassdoc"
 
 # -- Options for LaTeX output --------------------------------------------------
 
 latex_documents = [
-    ('index', u'Trends.Earth.tex', u'Trends.Earth Documentation', u'Conservation International', 'manual'),
-    ('training/tutorial_installation', u'Trends.Earth_Tutorial01_Installation.tex', u'Installation', u'Conservation International', 'howto'),
-    ('training/tutorial_run_all_subindicators', u'Trends.Earth_Tutorial02_Computing_Indicators.tex', u'Compute Sub-indicators', u'Conservation International', 'howto'),
-    ('training/tutorial_task_download', u'Trends.Earth_Tutorial03_Downloading_Results.tex', u'Downloading Results', u'Conservation International', 'howto'),
-    ('training/tutorial_custom_lpd', u'Trends.Earth_Tutorial04_Using_Custom_Productivity.tex', u'Using Custom Land Productivity Data', u'Conservation International', 'howto'),
-    ('training/tutorial_custom_landcover', u'Trends.Earth_Tutorial05_Using_Custom_Land_Cover.tex', u'Using Custom Land Cover Data', u'Conservation International', 'howto'),
-    ('training/tutorial_custom_soc', u'Trends.Earth_Tutorial06_Using_Custom_Soil_Carbon.tex', u'Using Custom Soil Organic Carbon Data', u'Conservation International', 'howto'),
-    ('training/tutorial_compute_sdg_indicator', u'Trends.Earth_Tutorial07_Computing_SDG_Indicator.tex', u'How to Compute the SDG Indicator', u'Conservation International', 'howto'),
-    ('training/tutorial_summary_table', u'Trends.Earth_Tutorial08_The_Summary_Table.tex', u'The Summary Table', u'Conservation International', 'howto'),
-    ('training/tutorial_load_basemap', u'Trends.Earth_Tutorial09_Loading_a_Basemap.tex', u'Loading a Basemap', u'Conservation International', 'howto'),
-    ('training/tutorial_forest_carbon', u'Trends.Earth_Tutorial10_Forest_Carbon.tex', u'Forest and Carbon Change Tool', u'Conservation International', 'howto'),
-    ('training/tutorial_compute_urban_indicator', u'Trends.Earth_Tutorial11_Urban_Change_SDG_Indicator.tex', u'Urban Change SDG 11.3.1', u'Conservation International', 'howto'),
+    (
+        "index",
+        u"Trends.Earth.tex",
+        u"Trends.Earth Documentation",
+        u"Conservation International",
+        "manual",
+    ),
 ]
 
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
-    'papersize': 'a4paper',
-    'preamble': u'''\\usepackage{fontspec}
-                    \\setmainfont{lmroman10-regular.otf}''',
+    "papersize": "a4paper",
+    "preamble": u"""\\usepackage{fontspec}
+                    \\setmainfont{lmroman10-regular.otf}""",
 }
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-latex_logo = '../resources/en/common/trends_earth_logo_bl_1200.png'
+latex_logo = "../resources/en/common/trends_earth_logo_bl_1200.png"
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
-#latex_use_parts = False
+# latex_use_parts = False
 
 # If true, show page references after internal links.
-#latex_show_pagerefs = False
+# latex_show_pagerefs = False
 
 # If true, show URL addresses after external links.
-#latex_show_urls = False
+# latex_show_urls = False
 
 # Documents to append as an appendix to all manuals.
-#latex_appendices = []
+# latex_appendices = []
 
 # If false, no module index is generated.
-#latex_domain_indices = True
+# latex_domain_indices = True
 
 
 # -- Options for manual page output --------------------------------------------
@@ -287,6 +304,11 @@ latex_logo = '../resources/en/common/trends_earth_logo_bl_1200.png'
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'TemplateClass', u'trends.earth documentation',
-     [u'Conservation International'], 1)
+    (
+        "index",
+        "TemplateClass",
+        u"trends.earth documentation",
+        [u"Conservation International"],
+        1,
+    )
 ]

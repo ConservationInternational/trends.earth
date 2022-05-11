7ba3c2b5a6a2b2392045b95625d45b1b54cd22eb
Alex Zvoleff <azvoleff@conservation.org>
2022-05-11 23:31:06 +0000

    Fix loading of parameter

--- gee/sdg-15-3-1-error-recode/src/main.py
+++ gee/sdg-15-3-1-error-recode/src/main.py
@@ -163,7 +163,7 @@ def run(params, logger):
     script_name = params['script_name']
     iso = params['iso']
     band_name = params['band_name']
-    band_sort_field = 'year_initial'
+    band_sort_field = params['band_sort_field']
     boundary_dataset = params.get('boundary_dataset', 'UN')
     productivity_dataset = params.get(
         'productivity_dataset', ProductivityMode.JRC_5_CLASS_LPD.value

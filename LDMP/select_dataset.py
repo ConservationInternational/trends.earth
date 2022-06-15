092900b9b9ad8501f00ec67896e758ff11ea3320
Alex Zvoleff <azvoleff@conservation.org>
2022-06-15 14:10:16 -0400

    Add region to false pos/negative window

--- LDMP/select_dataset.py
+++ LDMP/select_dataset.py
@@ -8,6 +8,9 @@ from qgis.PyQt import QtWidgets
 from qgis.PyQt import uic
 
 from . import tr
+from .conf import Setting
+from .conf import settings_manager
+from .settings import DlgSettings
 
 Ui_DlgSelectDS, _ = uic.loadUiType(
     str(Path(__file__).parents[0] / "gui/DlgSelectDS.ui")
@@ -17,9 +20,16 @@ ICON_PATH = os.path.join(os.path.dirname(__file__), "icons")
 
 
 class DlgSelectDataset(QtWidgets.QDialog, Ui_DlgSelectDS):
+    changed_region: QtCore.pyqtSignal = QtCore.pyqtSignal()
+
     def __init__(self, parent=None):
         super(DlgSelectDataset, self).__init__(parent)
         self.setupUi(self)
+
+        self.update_current_region()
+        self.region_button.setIcon(QtGui.QIcon(os.path.join(ICON_PATH, "wrench.svg")))
+        self.region_button.clicked.connect(self.run_settings)
+        self.changed_region.connect(self.populate_layers)
 
         self.combo_dataset.job_selected.connect(self.update_layers)
         self.populate_layers()
@@ -51,3 +61,14 @@ class DlgSelectDataset(QtWidgets.QDialog, Ui_DlgSelectDS):
 
     def soil_band(self):
         return self.combo_soil.get_current_band()
+
+    def update_current_region(self):
+        region = settings_manager.get_value(Setting.AREA_NAME)
+        self.region_la.setText(self.tr(f"Current region: {region}"))
+        self.changed_region.emit()
+
+    def run_settings(self):
+        dlg_settings = DlgSettings(parent=self)
+
+        if dlg_settings.exec_():
+            self.update_current_region()

ba231ab61692dcbc1e44fb8b1b9193c7fb11062c
Samweli <smwltwesa6@gmail.com>
2022-11-23 16:45:42 +0300

    fix for docker tests runner in linux

--- docker/trends-earth-test-pre-scripts.sh
+++ docker/trends-earth-test-pre-scripts.sh
@@ -7,5 +7,4 @@ rm -rf  /root/.local/share/QGIS/QGIS3/profiles/default/python/plugins/trends.ear
 ln -sf /tests_directory/LDMP /root/.local/share/QGIS/QGIS3/profiles/default/python/plugins/trends.earth
 
 
-pip3 install -r /tests_directory/requirements.txt
-pip3 install -r /tests_directory/requirements-dev.txt
+pip3 install -r /tests_directory/requirements-testing.txt

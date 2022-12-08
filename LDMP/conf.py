b1e29cb3fdbc7a6d8c77bd4ea02c8f3da6a206eb
vermeulendivan <vermeulendivan@gmail.com>
2022-12-08 13:41:31 +0200

    Syntax and format fixes

--- LDMP/conf.py
+++ LDMP/conf.py
@@ -29,8 +29,10 @@ OPTIONS_ICON = "trends_earth_logo_square_32x32.ico"
 OPTIONS_TITLE = "Trends.Earth"
 
 # Degradation Landcover matrix size settings
-RESIZE_NUM_ROWS = 5  # If there are equal to less than this value rows than the row height will be decreased
-MINIMUM_ROW_HEIGHT = 40  # This is the minimum row height value. 40 is the minimum at the moment, based on the font
+# If there are equal to less than this value rows than the row height will be decreased
+RESIZE_NUM_ROWS = 5
+# This is the minimum row height value. 40 is the cell font minimum
+MINIMUM_ROW_HEIGHT = 40
 
 class AreaSetting(enum.Enum):
     COUNTRY_REGION = "country_region"

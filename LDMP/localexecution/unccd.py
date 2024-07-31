5718443a1e2c27153ca92a861a5de7a3317a9b1e
Alex Zvoleff <azvoleff@conservation.org>
2024-07-30 21:43:52 -0400

    Fix insecure temporary file use

--- LDMP/localexecution/unccd.py
+++ LDMP/localexecution/unccd.py
@@ -115,14 +115,13 @@ def _set_affected_areas_only(in_file, out_file, schema):
 def _get_error_recode_polygons(in_file):
     ds_in = ogr.Open(in_file)
     layer_in = ds_in.GetLayer()
-    out_file = tempfile.mktemp(suffix=".geojson")
-    print(out_file)
-    out_ds = ogr.GetDriverByName("GeoJSON").CreateDataSource(out_file)
-    layer_out = out_ds.CopyLayer(layer_in, "error_recode")
-    del layer_out
-    del out_ds
-    with open(out_file) as f:
-        polys = ErrorRecodePolygons.Schema().load(json.load(f))
+    with tempfile.NamedTemporaryFile(suffix=".geojson") as temp:
+        out_ds = ogr.GetDriverByName("GeoJSON").CreateDataSource(temp.name)
+        layer_out = out_ds.CopyLayer(layer_in, "error_recode")
+        del layer_out
+        del out_ds
+        with open(temp.name) as f:
+            polys = ErrorRecodePolygons.Schema().load(json.load(f))
     return polys
 
 

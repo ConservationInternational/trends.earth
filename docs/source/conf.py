b8d53e44d0a489336ec626908bc017ba49186efb
Alex Zvoleff <azvoleff@gmail.com>
2024-10-22 15:24:11 -0400

    Remove unnecessary import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,6 @@
 import os
 from datetime import date
 
-import sphinx_rtd_theme
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.

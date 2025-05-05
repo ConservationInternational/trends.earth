ca4207edb0553de24559a359b2a1c12be69e2134
Alex Zvoleff <azvoleff@conservation.org>
2025-05-05 09:28:43 -0400

    Add script to calculate SDG indicator and status for periods without other layers

---
+++ gee/sdg-15-3-1-indicator/src/__init__.py
@@ -0,0 +1,15 @@
+__version__ = "2.1.17"
+
+import os
+import sys
+
+import rollbar
+
+rollbar.init(os.getenv("ROLLBAR_SCRIPT_TOKEN"), os.getenv("ENV"))
+
+
+def rollbar_except_hook(exc_type, exc_value, traceback):
+    # Report the issue to rollbar here.
+    rollbar.report_exc_info((exc_type, exc_value, traceback))
+    # display the error as normal here
+    sys.__excepthook__(exc_type, exc_value, traceback)

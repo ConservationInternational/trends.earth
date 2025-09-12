a39ddc1d30bc21c0f4c59e58e2c147e2d4e05d7c
Alex Zvoleff <azvoleff@conservation.org>
2025-09-12 09:41:27 -0400

    Fix path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -206,7 +206,7 @@ html_favicon = "trends_earth_logo_square_32x32.ico"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["static"]
+html_static_path = ["static", "../resources/en"]
 
 html_show_sourcelink = False
 

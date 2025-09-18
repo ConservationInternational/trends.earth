5fa8bfdb456275ed6e2ec1fe20743ab7f9279374
Dimas <dimas.ciputra@gmail.com>
2025-09-18 16:35:16 +0100

    update sdg-15-3-1-sub-indicators

--- gee/sdg-15-3-1-sub-indicators/src/main.py
+++ gee/sdg-15-3-1-sub-indicators/src/main.py
@@ -127,8 +127,6 @@ def _run_faowocat_for_period(params, max_workers, execution_id, logger):
                 [
                     "Soil organic carbon (degradation)",
                     "Land cover (degradation)",
-                    "Productivity trajectory (significance)",
-                    "Productivity state (degradation)",
                     config.FAO_WOCAT_LPD_BAND_NAME,
                 ]
             )

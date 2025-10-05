f18a2745814905d548ec1e7a152bf37944d18602
Alex Zvoleff <azvoleff@conservation.org>
2025-10-05 13:31:02 -0400

    Try a venv to fix dep failures

--- docker/trends-earth-test-pre-scripts.sh
+++ docker/trends-earth-test-pre-scripts.sh
@@ -14,10 +14,13 @@ ln -sf /tests_directory/LDMP /root/.local/share/QGIS/QGIS3/profiles/default/pyth
 # Older pip versions (< 24.0) don't properly handle PEP-517 builds from git/source
 # See: https://stackoverflow.com/questions/78034052/unknown-project-name-and-version-number-for-my-own-pip-package
 # Also upgrade setuptools and packaging to fix canonicalize_version() compatibility issues in QGIS 3.26
-# Use --ignore-installed to bypass system packages that might have invalid version strings (e.g., '0.8.0-final0')
-pip3 install --no-cache-dir --ignore-installed --upgrade pip setuptools packaging
+pip3 install --no-cache-dir --upgrade pip setuptools packaging
 
-# Install dependencies with --ignore-installed flag to handle distutils-installed packages
-# The QGIS container has blinker 1.4 installed via distutils which can't be uninstalled
-# Flask requires a newer blinker, so we ignore the old system package
-pip3 install --no-cache-dir --ignore-installed blinker -r /tests_directory/requirements-testing.txt
+# Install dependencies into a virtual environment to isolate from system packages
+# The QGIS 3.26 container has packages with invalid version strings (e.g., '0.8.0-final0')
+# that cause pip's dependency resolver to fail even with --ignore-installed
+# Using a venv completely isolates from the problematic system packages
+python3 -m venv --system-site-packages /tmp/test-venv
+source /tmp/test-venv/bin/activate
+pip install --no-cache-dir --upgrade pip setuptools packaging
+pip install --no-cache-dir -r /tests_directory/requirements-testing.txt

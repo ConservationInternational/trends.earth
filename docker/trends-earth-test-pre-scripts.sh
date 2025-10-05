d1aefb91957c8e0494a1abf955f91de68d088908
Alex Zvoleff <azvoleff@conservation.org>
2025-10-05 13:20:32 -0400

    Fix handling of old package

--- docker/trends-earth-test-pre-scripts.sh
+++ docker/trends-earth-test-pre-scripts.sh
@@ -14,7 +14,8 @@ ln -sf /tests_directory/LDMP /root/.local/share/QGIS/QGIS3/profiles/default/pyth
 # Older pip versions (< 24.0) don't properly handle PEP-517 builds from git/source
 # See: https://stackoverflow.com/questions/78034052/unknown-project-name-and-version-number-for-my-own-pip-package
 # Also upgrade setuptools and packaging to fix canonicalize_version() compatibility issues in QGIS 3.26
-pip3 install --upgrade pip setuptools packaging
+# Use --ignore-installed to bypass system packages that might have invalid version strings (e.g., '0.8.0-final0')
+pip3 install --no-cache-dir --ignore-installed --upgrade pip setuptools packaging
 
 # Install dependencies with --ignore-installed flag to handle distutils-installed packages
 # The QGIS container has blinker 1.4 installed via distutils which can't be uninstalled

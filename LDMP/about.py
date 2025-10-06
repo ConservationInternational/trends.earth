92a618d5a27189fc45b5248bd1cc86ee2b07e626
Alex Zvoleff <azvoleff@conservation.org>
2025-10-06 16:38:29 -0400

    Use setuptools-scm to track versioning for consistency

--- LDMP/about.py
+++ LDMP/about.py
@@ -29,18 +29,32 @@ class DlgAbout(QtWidgets.QDialog, Ui_DlgAbout):
 
         # Add version number to about dialog with clickable revision link
         github_repo_url = "https://github.com/ConservationInternational/trends.earth"
+
+        # Format the version string with git information
         if __revision__ and __revision__ != "unknown":
+            # Create a clickable link to the exact commit on GitHub
             revision_link = (
                 f'<a href="{github_repo_url}/commit/{__revision__}">{__revision__}</a>'
             )
+            # Format release date for display
+            if __release_date__ and __release_date__ != "unknown":
+                # Parse git date format (e.g., "2025-10-06 12:34:56 -0700")
+                # and display just the date portion
+                date_display = (
+                    __release_date__.split()[0]
+                    if " " in __release_date__
+                    else __release_date__
+                )
+            else:
+                date_display = "unknown"
+
             version = "{}<br>(revision {}, {})".format(
-                __version__, revision_link, __release_date__
+                __version__, revision_link, date_display
             )
         else:
-            # Fallback for unknown revision
-            version = "{}<br>(revision {}, {})".format(
-                __version__, __revision__, __release_date__
-            )
+            # Fallback for unknown revision (e.g., non-git installation)
+            version = "{}<br>(version from setuptools-scm)".format(__version__)
+
         self.textBrowser.setHtml(
             self.textBrowser.toHtml().replace("VERSION_NUMBER", version)
         )

8069259b2a424a73842a487a1fb4d84aacc438bf
Alex Zvoleff <azvoleff@conservation.org>
2025-10-08 15:13:45 -0400

    Make thread-safe

--- LDMP/logger.py
+++ LDMP/logger.py
@@ -1,7 +1,19 @@
 import typing
 
 import qgis.core
+from qgis.PyQt.QtCore import QTimer
 
 
 def log(message: str, level: typing.Optional[int] = 0):
-    qgis.core.QgsMessageLog.logMessage(message, tag="trends.earth", level=level)
+    """
+    Thread-safe logging function that can be called from any thread.
+    Uses QTimer.singleShot to ensure logging happens on the main thread.
+    This prevents segfaults when called from background threads on Windows.
+    """
+    # Use QTimer.singleShot with 0ms delay to queue the log message on the main thread
+    QTimer.singleShot(
+        0,
+        lambda: qgis.core.QgsMessageLog.logMessage(
+            message, tag="trends.earth", level=level
+        ),
+    )
